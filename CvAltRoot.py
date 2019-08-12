9974187ca43741c8e5cb84a39586ffdce4b56472
alberts2 <alberts2@live.de>
2019-08-12 17:40:56 +0200

    Added all necessary files to the root

---
+++ CvAltRoot.py
@@ -0,0 +1,41 @@
+## CvAltRoot
+##
+## Tells Rise of Mankind where to locate its files when it cannot find them normally.
+## This is common when using the /AltRoot Civ4 feature or sometimes on
+## non-English operating systems and Windows Vista.
+##
+## HOW TO USE
+##
+## 1. Change the text in the quotes below to match the full path to the
+##    "Beyond the Sword" folder that contains the CivilizationIV.ini file.
+##    Make sure to use forward slashes (/), even for Windows paths.
+##
+##    Windows XP: "C:/Documents and Settings/[UserName]/My Documents/My Games/Beyond the Sword"
+##
+##    Windows Vista: "C:/Users/[UserName]/Documents/My Games/Beyond the Sword"
+##
+##    MacOS: "/home/[UserName]/Documents/Beyond the Sword"
+##
+##
+## 2. Copy this file to the "Python" folder.
+##    When Rise of Mankind is installed as a mod, the folder is "Assets/Python".
+##
+##
+## Note!
+##
+## if above method doesn't help, locate the "Beyond the Sword" installation folder. 
+## This location is not in the documents folders, instead it's usually in 
+## "Program Files" ie. "C:/Program Files/Firaxis/Sid Meier's Civilization 4/Beyond the Sword"
+## The folder usually contains "_Civ4config" shortcut (on Windows systems). Once you have
+## located this folder, follow the step 2 again. Usually when [username] in above examples
+## contains non-english letters you will have to use the installation folder location.
+## 
+##
+## Copyright (c) 2008 The BUG Mod.
+##
+## Author: EmperorFool
+##
+## Modified for Rise of Mankind: Zappara
+##
+
+rootDir = "C:/Documents and Settings/[UserName]/My Documents/My Games/Beyond the Sword"
