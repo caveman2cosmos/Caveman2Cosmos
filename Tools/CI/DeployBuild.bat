ec2eb3eb62886160f5502f211913dab8bd0c89e6
billw2012 <billw2011@gmail.com>
2019-08-18 18:38:42 +0100

    Enabling all the deployment script bits

--- Tools/CI/DeployBuild.bat
+++ Tools/CI/DeployBuild.bat
@@ -4,56 +4,56 @@ PUSHD "%~dp0..\.."
 echo C2C %APPVEYOR_BUILD_VERSION% DEPLOYMENT
 echo.
 
-REM echo Packing FPKs...
-REM call Tools\PackFPKs.bat CI
-REM if not errorlevel 0 (
-REM     echo Packing FPKs failed, aborting deployment!
-REM     exit /B 1
-REM )
+echo Packing FPKs...
+call Tools\PackFPKs.bat CI
+if not errorlevel 0 (
+    echo Packing FPKs failed, aborting deployment!
+    exit /B 1
+)
 
-REM echo Building FinalRelease DLL...
-REM call Tools\MakeDLLFinalRelease.bat
-REM if not errorlevel 0 (
-REM     echo Building FinalRelease DLL failed, aborting deployment!
-REM     exit /B 2
-REM )
-REM call Tools\CI\DoSourceIndexing.bat
+echo Building FinalRelease DLL...
+call Tools\MakeDLLFinalRelease.bat
+if not errorlevel 0 (
+    echo Building FinalRelease DLL failed, aborting deployment!
+    exit /B 2
+)
+call Tools\CI\DoSourceIndexing.bat
 
 cd /d "%~dp0..\.."
-REM if not exist "%build_dir%" goto :checkout
-REM rmdir /Q /S "%build_dir%"
+if not exist "%build_dir%" goto :checkout
+rmdir /Q /S "%build_dir%"
 
-REM :checkout
-REM echo Checking out SVN working copy for deployment...
-REM svn checkout --username=%svn_user% %svn_url% "%build_dir%"
+:checkout
+echo Checking out SVN working copy for deployment...
+svn checkout --username=%svn_user% %svn_url% "%build_dir%"
 
-REM :: HERE IS WHERE YOU ADJUST WHAT TO PUT IN THE BUILD
-REM echo Updating SVN working copy from git...
-REM set ROBOCOPY_FLAGS=/MIR /NFL /NDL /NJH /NJS /NS /NC
-REM robocopy Assets "%build_dir%\Assets" %ROBOCOPY_FLAGS%
-REM robocopy PrivateMaps "%build_dir%\PrivateMaps" %ROBOCOPY_FLAGS%
-REM robocopy Resource "%build_dir%\Resource" %ROBOCOPY_FLAGS%
-REM xcopy Caveman2Cosmos.ini "%build_dir%" /R /Y
-REM xcopy "Caveman2Cosmos Config.ini" "%build_dir%" /R /Y
+:: HERE IS WHERE YOU ADJUST WHAT TO PUT IN THE BUILD
+echo Updating SVN working copy from git...
+set ROBOCOPY_FLAGS=/MIR /NFL /NDL /NJH /NJS /NS /NC
+robocopy Assets "%build_dir%\Assets" %ROBOCOPY_FLAGS%
+robocopy PrivateMaps "%build_dir%\PrivateMaps" %ROBOCOPY_FLAGS%
+robocopy Resource "%build_dir%\Resource" %ROBOCOPY_FLAGS%
+xcopy Caveman2Cosmos.ini "%build_dir%" /R /Y
+xcopy "Caveman2Cosmos Config.ini" "%build_dir%" /R /Y
 
-REM echo Detecting working copy changes...
-REM PUSHD "%build_dir%"
-REM set "SVN=C:\Program Files\SlikSvn\bin\svn.exe"
-REM "%SVN%" status | findstr /R "^!" > ..\missing.list
-REM for /F "tokens=* delims=! " %%A in (..\missing.list) do (svn delete "%%A")
-REM del ..\missing.list 2>NUL
-REM "%SVN%" add * --force
-REM REM svn status | findstr /R "^?" > ..\added.list
-REM REM for /F "tokens=* delims=? " %%A in (..\added.list) do (svn add "%%A")
-REM REM del ..\added.list 2>NUL
+echo Detecting working copy changes...
+PUSHD "%build_dir%"
+set "SVN=C:\Program Files\SlikSvn\bin\svn.exe"
+"%SVN%" status | findstr /R "^!" > ..\missing.list
+for /F "tokens=* delims=! " %%A in (..\missing.list) do (svn delete "%%A")
+del ..\missing.list 2>NUL
+"%SVN%" add * --force
+REM svn status | findstr /R "^?" > ..\added.list
+REM for /F "tokens=* delims=? " %%A in (..\added.list) do (svn add "%%A")
+REM del ..\added.list 2>NUL
 
-REM REM for /f "tokens=2*" %%i in ('svn status "%1" ^| find "?"') do svn add "%%i"
-REM REM for /f "tokens=2*" %%i in ('svn status "%1" ^| find "!"') do svn delete "%%i"
+REM for /f "tokens=2*" %%i in ('svn status "%1" ^| find "?"') do svn add "%%i"
+REM for /f "tokens=2*" %%i in ('svn status "%1" ^| find "!"') do svn delete "%%i"
 
 
-REM echo Commiting new build to SVN...
-REM "%SVN%" commit -m "C2C %APPVEYOR_BUILD_VERSION%" --username %svn_user% --password %svn_pass% --non-interactive
-REM POPD
+echo Commiting new build to SVN...
+"%SVN%" commit -m "C2C %APPVEYOR_BUILD_VERSION%" --username %svn_user% --password %svn_pass% --non-interactive
+POPD
 
 7z a -r -x!.svn "%release_prefix%-%APPVEYOR_BUILD_VERSION%.zip" "%build_dir%\*.*"
 7z a -x!.svn "%release_prefix%-CvGameCoreDLL-%APPVEYOR_BUILD_VERSION%.zip" "%build_dir%\Assets\CvGameCoreDLL.*"
