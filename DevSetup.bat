cf567a64cf5f7544ac660bb892a2a00071fb2451
billw2012 <billw2011@gmail.com>
2019-08-21 20:51:02 +0100

    Added git client download and install to DevSetup

--- DevSetup.bat
+++ DevSetup.bat
@@ -1,3 +1,45 @@
+::::::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights V2
+::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
+
 @echo off
 
 echo.
@@ -12,6 +54,7 @@ echo.
 pause
 
 PUSHD "."
+call "%~dp0\Tools\Setup\InstallGit.bat"
 call "%~dp0\Tools\Install.bat"
 call "%~dp0\Tools\_MakeDLL.bat" build Assert
 POPD
---
+++ Tools/Setup/DownloadAndInstallGit.ps1
@@ -0,0 +1,73 @@
+function FindGitExe {
+    if (![String]::IsNullOrEmpty($gitPath)) {
+        return $gitPath
+    }
+  
+    $gitPath = Get-Command git.exe| Select-Object -ExpandProperty Definition  
+    if (![String]::IsNullOrEmpty($gitPath)) {
+        return $gitPath
+    }
+
+    $suffix = "\git\bin\git.exe"
+    
+    $gitexe = ${env:ProgramFiles} + $suffix
+    if (Test-Path $gitexe) {
+        return $gitexe
+    }
+    
+    if( [IntPtr]::size -eq 4 ) {
+        return $null
+    }
+    
+    $gitexe = ${env:ProgramFiles(x86)} + $suffix
+    if (Test-Path $gitexe) {
+        return $gitexe
+    }
+    
+    return $null
+}
+
+function downloadFile($url, $targetFile)
+{ 
+    "Downloading $url" 
+    $uri = New-Object "System.Uri" "$url" 
+    $request = [System.Net.HttpWebRequest]::Create($uri) 
+    $request.set_Timeout(15000) #15 second timeout 
+    $response = $request.GetResponse() 
+    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024) 
+    $responseStream = $response.GetResponseStream() 
+    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create 
+    $buffer = new-object byte[] 10KB 
+    $count = $responseStream.Read($buffer,0,$buffer.length) 
+    $downloadedBytes = $count 
+    while ($count -gt 0) 
+    { 
+        [System.Console]::CursorLeft = 0 
+        [System.Console]::Write("Downloaded {0}K of {1}K", [System.Math]::Floor($downloadedBytes/1024), $totalLength) 
+        $targetStream.Write($buffer, 0, $count) 
+        $count = $responseStream.Read($buffer,0,$buffer.length) 
+        $downloadedBytes = $downloadedBytes + $count 
+    } 
+    "`nFinished Download" 
+    $targetStream.Flush()
+    $targetStream.Close() 
+    $targetStream.Dispose() 
+    $responseStream.Dispose() 
+}
+
+$gitexe = FindGitExe
+if (!$gitexe) {
+    "Git command line not found! It will now be downloaded and installed for you, if you wish to cancel then close the window."
+    "Otherwise press any key to continue with download and install..."
+    [void][System.Console]::ReadKey($true)
+
+    if([Environment]::Is64BitOperatingSystem) {
+        downloadFile "https://github.com/git-for-windows/git/releases/download/v2.23.0.windows.1/Git-2.23.0-64-bit.exe" "git_installer.exe"
+    }
+    else {
+        downloadFile "https://github.com/git-for-windows/git/releases/download/v2.23.0.windows.1/Git-2.23.0-32-bit.exe" "git_installer.exe"
+    }
+    
+    "Waiting for installer to finish..."
+    Start-Process -FilePath "git_installer.exe" -ArgumentList "/SILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=git_install.inf" -Wait
+}
