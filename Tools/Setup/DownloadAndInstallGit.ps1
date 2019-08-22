79797e9e78f0027d618810e51ecb59dc24e83e55
billw2019 <53123965+billw2019@users.noreply.github.com>
2019-08-22 23:21:02 +0100

    Convert git download to use wget

--- Tools/Setup/DownloadAndInstallGit.ps1
+++ Tools/Setup/DownloadAndInstallGit.ps1
@@ -27,35 +27,6 @@ function FindGitExe {
     return $null
 }
 
-function downloadFile($url, $targetFile)
-{ 
-    "Downloading $url" 
-    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-    $uri = New-Object "System.Uri" "$url" 
-    $request = [System.Net.HttpWebRequest]::Create($uri) 
-    $request.set_Timeout(15000) #15 second timeout 
-    $response = $request.GetResponse() 
-    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024) 
-    $responseStream = $response.GetResponseStream() 
-    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create 
-    $buffer = new-object byte[] 10KB 
-    $count = $responseStream.Read($buffer,0,$buffer.length) 
-    $downloadedBytes = $count 
-    while ($count -gt 0) 
-    { 
-        [System.Console]::CursorLeft = 0 
-        [System.Console]::Write("Downloaded {0}K of {1}K", [System.Math]::Floor($downloadedBytes/1024), $totalLength) 
-        $targetStream.Write($buffer, 0, $count) 
-        $count = $responseStream.Read($buffer,0,$buffer.length) 
-        $downloadedBytes = $downloadedBytes + $count 
-    } 
-    "`nFinished Download" 
-    $targetStream.Flush()
-    $targetStream.Close() 
-    $targetStream.Dispose() 
-    $responseStream.Dispose() 
-}
-
 $gitexe = FindGitExe
 if (!$gitexe) {
     "Git command line not found! It will now be downloaded and installed for you, if you wish to cancel then close the window."
@@ -63,10 +34,10 @@ if (!$gitexe) {
     [void][System.Console]::ReadKey($true)
 
     if([Environment]::Is64BitOperatingSystem) {
-        downloadFile "https://github.com/git-for-windows/git/releases/download/v2.23.0.windows.1/Git-2.23.0-64-bit.exe" "git_installer.exe"
+        & ".\wget.exe" -O "git_installer.exe" "https://github.com/git-for-windows/git/releases/download/v2.23.0.windows.1/Git-2.23.0-64-bit.exe" --quiet --show-progress
     }
     else {
-        downloadFile "https://github.com/git-for-windows/git/releases/download/v2.23.0.windows.1/Git-2.23.0-32-bit.exe" "git_installer.exe"
+        & ".\wget.exe" -O "git_installer.exe" "https://github.com/git-for-windows/git/releases/download/v2.23.0.windows.1/Git-2.23.0-32-bit.exe" --quiet --show-progress
     }
     
     "Waiting for installer to finish..."
