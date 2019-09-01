824bf507a840b8b0989d197d04076d78e8c54e30
alberts2 <alberts2@live.de>
2019-09-01 22:10:00 +0200

    Fix IDValueMap and it's usages
    Reverted some changes from svn10994.
    These changes broke the adaptValueToGame functionality.

--- Sources/IDValueMap.h
+++ Sources/IDValueMap.h
@@ -12,7 +12,6 @@
 #define IDVALUEMAP_H
 
 #include "CvXMLLoadUtility.h"
-#include "CvString.h"
 
 // ValueType will usually be int, only value types that are supported by FDataStreamBase as overloaded read and write will work without template specialization
 // The maps are assumed to be small, so a vector of pairs is used
@@ -20,14 +19,12 @@ template <class ValueType, ValueType& defaultValue>
 class IDValueMap
 {
 public:
-	static uint getIDForString(const char* szType);
-
 	void read(FDataStreamBase* pStream)
 	{
 		unsigned int iSize = 0;
 		pStream->Read(&iSize);
 		m_map.resize(iSize);
-		for (unsigned int i=0; i<iSize; i++)
+		for (unsigned int i = 0; i < iSize; i++)
 		{
 			pStream->Read(&(m_map[i].first));
 			pStream->Read(&(m_map[i].second));
@@ -37,7 +34,7 @@ public:
 	void write(FDataStreamBase* pStream)
 	{
 		pStream->Write(m_map.size());
-		for (unsigned int i=0; i<m_map.size(); i++)
+		for (unsigned int i = 0; i < m_map.size(); i++)
 		{
 			pStream->Write(m_map[i].first);
 			pStream->Write(m_map[i].second);
@@ -56,26 +53,25 @@ public:
 				if (pXML->TryMoveToXmlFirstChild())
 				{
 					pXML->GetXmlVal(szTextVal);
-					uint iID = getIDForString(szTextVal);
+					int iID = GC.getOrCreateInfoTypeForString(szTextVal);
 					ValueType val = defaultValue;
 					pXML->GetNextXmlVal(&val);
 					setValue(iID, val);
-					
+
 					pXML->MoveToXmlParent();
 				}
-			}
-			while (pXML->TryMoveToXmlNextSibling());
+			} while (pXML->TryMoveToXmlNextSibling());
 			pXML->MoveToXmlParent();
 		}
 	}
 
 	void copyNonDefaults(IDValueMap<ValueType, defaultValue>* pMap, CvXMLLoadUtility* pXML)
 	{
-		for (unsigned int i=0; i<pMap->m_map.size(); i++)
+		for (unsigned int i = 0; i < pMap->m_map.size(); i++)
 		{
 			bool bNotFound = true;
 			int iID = pMap->m_map[i].first;
-			for (unsigned int j=0; j<m_map.size(); j++)
+			for (unsigned int j = 0; j < m_map.size(); j++)
 			{
 				if (iID == m_map[j].first)
 				{
@@ -92,24 +88,24 @@ public:
 
 	void getCheckSum(unsigned int& iSum)
 	{
-		for (unsigned int i=0; i<m_map.size(); i++)
+		for (unsigned int i = 0; i < m_map.size(); i++)
 		{
 			CheckSum(iSum, m_map[i].first);
 			CheckSum(iSum, m_map[i].second);
 		}
 	}
 
-	ValueType getValue(uint iID) const
+	ValueType getValue(int iID) const
 	{
-		for (uint i = 0; i < m_map.size(); i++)
+		for (unsigned int i = 0; i < m_map.size(); i++)
 			if (m_map[i].first == iID)
 				return m_map[i].second;
 		return defaultValue;
 	}
 
-	void setValue(uint iID, ValueType val)
+	void setValue(int iID, ValueType val)
 	{
-		for (uint i = 0; i < m_map.size(); i++)
+		for (unsigned int i = 0; i < m_map.size(); i++)
 		{
 			if (m_map[i].first == iID)
 			{
@@ -122,8 +118,7 @@ public:
 
 
 protected:
-	std::vector<std::pair<uint,ValueType> > m_map;
-	static std::vector<std::pair<uint,CvString> > s_IDmap;
+	std::vector<std::pair<int, ValueType> > m_map;
 };
 
 extern int g_iPercentDefault;
@@ -131,23 +126,5 @@ extern int g_iModifierDefault;
 
 typedef IDValueMap<int, g_iPercentDefault> IDValueMapPercent;
 typedef IDValueMap<int, g_iModifierDefault> IDValueMapModifier;
-
-template <class ValueType, ValueType& defaultValue>
-std::vector<std::pair<uint,CvString> > IDValueMap<ValueType, defaultValue>::s_IDmap;
-
-template <class ValueType, ValueType& defaultValue>
-uint IDValueMap<ValueType, defaultValue>::getIDForString(const char* szType)
-{
-	for (std::vector<std::pair<uint,CvString> >::iterator it = s_IDmap.begin(); it != s_IDmap.end(); ++it)
-	{
-		if (it->second.CompareNoCase(szType) == 0)
-		{
-			return it->first;
-		}
-	}
-	uint uiID = s_IDmap.size();
-	s_IDmap.push_back(std::make_pair(uiID, szType));
-	return uiID;
-}
 
 #endif
