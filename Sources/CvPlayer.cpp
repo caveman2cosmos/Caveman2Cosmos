fd1d5fe2eda9b327a2740f2f9f3e54cf76d69923
Thunderbrd <newrav@hotmail.com>
2019-08-31 20:15:36 -0700

    [*]War Weariness was correct but the percentage calculation of unhappiness from it was off by a factor of an extra /100.  Corrected.

--- Sources/CvPlayer.cpp
+++ Sources/CvPlayer.cpp
@@ -15050,6 +15050,7 @@ void CvPlayer::updateWarWearinessPercentAnger()
 	int iI;
 
 	iNewWarWearinessPercentAnger = 0;
+	int iEquation = 0;
 
 	if (!isNPC() && !isMinorCiv())
 	{
@@ -15060,7 +15061,10 @@ void CvPlayer::updateWarWearinessPercentAnger()
 			{
 				if (kTeam.isAtWar(getTeam()))
 				{
-					iNewWarWearinessPercentAnger += (GET_TEAM(getTeam()).getWarWeariness((TeamTypes)iI) * std::max(0, 100 + kTeam.getEnemyWarWearinessModifier())) / 10000;
+					iEquation = 0;
+					iEquation = (GET_TEAM(getTeam()).getWarWeariness((TeamTypes)iI) * std::max(0, 100 + kTeam.getEnemyWarWearinessModifier()));
+					iEquation /= 1000000;
+					iNewWarWearinessPercentAnger += iEquation;
 				}
 			}
 		}
