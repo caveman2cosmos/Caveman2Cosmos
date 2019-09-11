33b42f704204e7856a15cce917a2937dc647dbcd
billw2012 <billw2011@gmail.com>
2019-09-11 13:49:05 +0100

    Autocorrect supports early exit and rules without suggestions better

---
+++ Tools/Autocorrect/Autocorrect.bat
@@ -0,0 +1,4 @@
+@echo off
+PUSHD "%~dp0"
+python Autocorrect.py %*
+POPD
--- Tools/Autocorrect/Autocorrect.py
+++ Tools/Autocorrect/Autocorrect.py
@@ -30,6 +30,9 @@ def namespace(element):
     m = re.match(r'\{(.*)\}', element.tag)
     return m.group(1) if m else ''
 
+# define Python user-defined exceptions
+class ExitEarly(Exception):
+   pass
 
 def autocorrect(files, mode):
     ignore_words = load_string_list('ignore_word_list.txt')
@@ -42,14 +45,18 @@ def autocorrect(files, mode):
         root = etree.parse(filename).getroot()
         #nspace = namespace(root)
         nsmap = {'': namespace(root)}
-        # .register_namespace('', nspace)
-        for text_elem in root.findall('TEXT', nsmap):
-            tag_elem = text_elem.find('Tag', nsmap)
-            if tag_elem is not None and tag_elem.text not in ignore_tags:
-                eng_elem = text_elem.find('English', nsmap)
-                if eng_elem is not None:
-                    autocorrect_element(
-                        eng_elem, tag_elem.text, ignore_words, ignore_tags, ignore_rules, mode, '  ')
+
+        try:
+            # .register_namespace('', nspace)
+            for text_elem in root.findall('TEXT', nsmap):
+                tag_elem = text_elem.find('Tag', nsmap)
+                if tag_elem is not None and tag_elem.text not in ignore_tags:
+                    eng_elem = text_elem.find('English', nsmap)
+                    if eng_elem is not None:
+                        autocorrect_element(
+                            eng_elem, tag_elem.text, ignore_words, ignore_tags, ignore_rules, mode, '  ')
+        except ExitEarly:
+            pass
 
         if mode != Mode.DETECT:
             etree.ElementTree(root).write(filename, encoding="utf-8", xml_declaration=True, pretty_print=True)
@@ -71,16 +78,23 @@ def autocorrect_element(eng_elem, tag, ignore_words, ignore_tags, ignore_rules, 
             # Make sure its sorted by offset (it should be already)
             matches.sort(key=lambda x: x['offset'])
 
-            print(indent + '%d errors found in text "%s":' %
-                  (len(matches), eng_elem.text))
+            print(indent + u'{0} errors found in text "{1}":'.format(len(matches), eng_elem.text))
             indent = indent + '    '
+
             # We need to keep track of changes introduced by corrections to
             # correctly apply consecutive ones
             corrected_text, corrected_text_with_markers = apply_corrections(
                 eng_elem.text, matches)
-            print(indent + 'Corrected text: %s' % (corrected_text_with_markers))
+
+            if eng_elem.text != corrected_text:
+                print(indent + u'Corrected text: {0}'.format(corrected_text_with_markers))
+
             if mode == Mode.INTERACTIVE:
-                print(indent + 'Accept all (return), Skip (s), Interactive (space), Ignore %s (x)?' % tag),
+                if eng_elem.text != corrected_text:
+                    print(indent + 'Accept all (return), Skip (s), Interactive (space), Ignore %s (x), Exit (esc)?' % tag),
+                else:
+                    print(indent + 'Skip (s), Interactive (space), Ignore %s (x), Exit (esc)?' % tag),
+
                 key = msvcrt.getch()
                 print('')
                 if key == b'\r':
@@ -90,13 +104,15 @@ def autocorrect_element(eng_elem, tag, ignore_words, ignore_tags, ignore_rules, 
                     print(indent + 'Entering interactive mode')
                     corrected_text, corrected_text_with_markers = apply_corrections_interactive(
                         eng_elem.text, matches, ignore_words, ignore_rules, indent + '  ')
-                    print(indent + 'Corrected text: %s' % (corrected_text_with_markers))
+                    print(indent + u'Corrected text: {0}'.format(corrected_text_with_markers))
                     eng_elem.text = corrected_text
                 elif key == b'x':
                     ignore_tags.append(tag)
                     print(indent + 'Added %s to the global ignore list' % tag)
-                else: # key == b's':
+                elif key == b's':
                     print(indent + 'Skipping %s' % tag)
+                else:
+                    raise ExitEarly
             elif mode == Mode.AUTOMATIC:
                 eng_elem.text = corrected_text
 
@@ -144,19 +160,21 @@ def create_error_underlines(context):
     length = context['length']
     return ' ' * offset + '^' * length
 
-# def replace(replacement, match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks):
-#     offset = match['offset'] + offs_adj
-#     length = match['length']
-#     orig_text_len = len(corrected_text)
-#     corrected_text = corrected_text[:offset] + replacement + corrected_text[offset+length:]
-#     offs_adj = offs_adj + (len(corrected_text) - orig_text_len)
+def replace(replacement, match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks):
+    # nonlocal offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks
 
-#     offset = match['offset'] + offs_adj_mrks
-#     orig_text_len = len(corrected_text_mrks)
-#     corrected_text_mrks = corrected_text_mrks[:offset] + replacement + corrected_text_mrks[offset+length:]
-#     offs_adj_mrks = offs_adj_mrks + (len(corrected_text_mrks) - orig_text_len)
+    offset = match['offset'] + offs_adj
+    length = match['length']
+    orig_text_len = len(corrected_text)
+    corrected_text = corrected_text[:offset] + replacement + corrected_text[offset+length:]
+    offs_adj = offs_adj + (len(corrected_text) - orig_text_len)
 
-#     return offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks
+    offset = match['offset'] + offs_adj_mrks
+    orig_text_len = len(corrected_text_mrks)
+    corrected_text_mrks = corrected_text_mrks[:offset] + replacement + corrected_text_mrks[offset+length:]
+    offs_adj_mrks = offs_adj_mrks + (len(corrected_text_mrks) - orig_text_len)
+
+    return offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks
 
 def apply_corrections_interactive(text, matches, ignore_words, ignore_rules, indent, marker='#'):
     # We need to keep track of changes introduced by corrections to
@@ -167,21 +185,6 @@ def apply_corrections_interactive(text, matches, ignore_words, ignore_rules, ind
     offs_adj_mrks = 0
     corrected_text_mrks = text
 
-    def replace(replacement, match):
-        nonlocal offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks
-
-        offset = match['offset'] + offs_adj
-        length = match['length']
-        orig_text_len = len(corrected_text)
-        corrected_text = corrected_text[:offset] + replacement + corrected_text[offset+length:]
-        offs_adj = offs_adj + (len(corrected_text) - orig_text_len)
-
-        offset = match['offset'] + offs_adj_mrks
-        orig_text_len = len(corrected_text_mrks)
-        corrected_text_mrks = corrected_text_mrks[:offset] + replacement + corrected_text_mrks[offset+length:]
-        offs_adj_mrks = offs_adj_mrks + (len(corrected_text_mrks) - orig_text_len)
-        return
-
     for match in matches:
         offset = match['offset'] + offs_adj
         length = match['length']
@@ -191,41 +194,47 @@ def apply_corrections_interactive(text, matches, ignore_words, ignore_rules, ind
             context = match['context']
             error_underline = create_error_underlines(context)
             prefix = indent + '%s (%s [%s]): ' % (match['message'], match['rule']['issueType'], match['rule']['id'])
-            print('%s%s' % (prefix, context['text']))
+            print(u'{0}{1}'.format(prefix, context['text']))
             prefix_space = ' ' * len(prefix)
-            print('%s%s' % (prefix_space, error_underline))
+            print(u'{0}{1}'.format(prefix_space, error_underline))
 
-            if 'replacements' in match and len(match['replacements']) > 0:
-                print(indent + 'Suggestions: %s' %
-                    ', '.join(['[%d] %s' % (idx, v['value']) for idx, v in enumerate(match['replacements'][:10])]))
-                print(indent + '> Accept best (return), Add to ignore (x), Select suggestion (0-9), Skip (s), Ignore rule (e), Custom entry (c)?'),
-                key = msvcrt.getch()
-                print('\n')
+            can_fix = 'replacements' in match and len(match['replacements']) > 0
+            if can_fix:
+                print(indent + u'Suggestions: {0}'.format(
+                    u', '.join([u'[{0}] {1}'.format(idx, v['value']) for idx, v in enumerate(match['replacements'][:10])])))
+                print(indent + '> Accept best (return), Add to ignore (x), Select suggestion (0-9), Skip (s), Ignore rule (e), Custom entry (c), Exit(esc)?'),
+            else:
+                print(indent + '> Skip (s), Ignore rule (e), Exit(esc)?'),
 
-                if key == b'\r' or (key >= b'0' and key <= b'9'):
-                    index = 0
-                    if key >= b'0' and key <= b'9':
-                        index = ord(key) - ord(b'0')
-                    # offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(match['replacements'][index]['value'], match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks)
-                    replace(match['replacements'][index]['value'], match)
-                    # replacement = match['replacements'][index]['value']
-                    # orig_text_len = len(corrected_text)
-                    # corrected_text = corrected_text[:offset] + replacement + corrected_text[offset+length:]
-                    # offs_adj = offs_adj + (len(corrected_text) - orig_text_len)
+            key = msvcrt.getch()
+            print('\n')
 
-                    # offset = match['offset'] + offs_adj_mrks
-                    # orig_text_len = len(corrected_text_mrks)
-                    # corrected_text_mrks = corrected_text_mrks[:offset] + replacement + corrected_text_mrks[offset+length:]
-                    # offs_adj_mrks = offs_adj_mrks + (len(corrected_text_mrks) - orig_text_len)
-                elif key == b'x':
-                    ignore_words.append(to_replace)
-                elif key == b'e':
-                    ignore_rules.append(match['rule']['id'])
-                elif key == b'c':
-                    #offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(input('Enter text to replace %s > ' % to_replace), match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks)
-                    replace(input('Enter text to replace %s > ' % to_replace), match)
-                else: #key == b's'
-                    pass
+            if can_fix and (key == b'\r' or (key >= b'0' and key <= b'9')):
+                index = 0
+                if key >= b'0' and key <= b'9':
+                    index = ord(key) - ord(b'0')
+                # offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(match['replacements'][index]['value'], match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks)
+                offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(match['replacements'][index]['value'], match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks)
+                # replacement = match['replacements'][index]['value']
+                # orig_text_len = len(corrected_text)
+                # corrected_text = corrected_text[:offset] + replacement + corrected_text[offset+length:]
+                # offs_adj = offs_adj + (len(corrected_text) - orig_text_len)
+
+                # offset = match['offset'] + offs_adj_mrks
+                # orig_text_len = len(corrected_text_mrks)
+                # corrected_text_mrks = corrected_text_mrks[:offset] + replacement + corrected_text_mrks[offset+length:]
+                # offs_adj_mrks = offs_adj_mrks + (len(corrected_text_mrks) - orig_text_len)
+            elif can_fix and key == b'x':
+                ignore_words.append(to_replace)
+            elif key == b'e':
+                ignore_rules.append(match['rule']['id'])
+            elif can_fix and key == b'c':
+                #offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(input('Enter text to replace %s > ' % to_replace), match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks)
+                offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(input(u'Enter text to replace {0} > '.format(to_replace)), match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks)
+            elif key == b's':
+                pass
+            else:
+                raise ExitEarly
 
     return corrected_text, corrected_text_mrks
 
