29c3aac226bc5646f3a7404564da62689a5b5352
billw2012 <billw2011@gmail.com>
2019-09-17 19:37:34 +0100

    Remove pauses from autobuild good path

--- Tools/_BootDLLCheck.bat
+++ Tools/_BootDLLCheck.bat
@@ -4,7 +4,6 @@ PUSHD "%~dp0"
 :: Call iterative build without staging the DLL (we couldn't anyway as Civ will have locked it)
 call _MakeDLL.bat autobuild %1
 if errorlevel 1 goto :build_failed
-pause
 :: Compare the built DLL with the staged one
 fc ..\Build\%1\CvGameCoreDLL.dll ..\Assets\CvGameCoreDLL.dll > nul
 :: If there is no change then we can exit
@@ -13,14 +12,19 @@ goto :no_change
 :changed
 :: If there is a change then kill Civ, stage the new DLL and restart it again
 call :banner
-echo   %1 DLL UPDATED!
+echo [32m"******************************************************"[0m
+echo [32m"*     _   _ ____  ____    _  _____ _____ ____  _     *"[0m
+echo [32m"*    | | | |  _ \|  _ \  / \|_   _| ____|  _ \| |    *"[0m
+echo [32m"*    | | | | |_) | | | |/ _ \ | | |  _| | | | | |    *"[0m
+echo [32m"*    | |_| |  __/| |_| / ___ \| | | |___| |_| |_|    *"[0m
+echo [32m"*     \___/|_|   |____/_/   \_\_| |_____|____/(_)    *"[0m
+echo [32m"*                                                    *"[0m
+echo [32m"******************************************************"[0m
+echo   If you use Steam then you will need to run the game 
+echo   again yourself!
 echo.
-echo   Press any key to restart the game and apply the changes ...
-echo.
-echo   (If you use Steam then you will need to run the game again yourself)
-echo.
+timeout 5
 
-pause > nul
 taskkill /F /IM Civ4BeyondSword.exe
 call _MakeDLL.bat build %1 stage
 
@@ -33,21 +37,18 @@ goto :done
 
 :build_failed
 call :banner
-echo.
-echo    ллЛ   ллЛллллллЛ ллллллЛ  лллллЛ ллллллллЛлллллллЛ   
-echo    ллК   ллКллЩЭЭллЛллЩЭЭллЛллЩЭЭллЛШЭЭллЩЭЭМллЩЭЭЭЭМ   
-echo    ллК   ллКллллллЩМллК  ллКлллллллК   ллК   лллллЛ     
-echo    ллК   ллКллЩЭЭЭМ ллК  ллКллЩЭЭллК   ллК   ллЩЭЭМ     
-echo    ШллллллЩМллК     ллллллЩМллК  ллК   ллК   лллллллЛ   
-echo     ШЭЭЭЭЭМ ШЭМ     ШЭЭЭЭЭМ ШЭМ  ШЭМ   ШЭМ   ШЭЭЭЭЭЭМ   
-echo.
-echo      лллллллЛ лллллЛ ллЛллЛ     лллллллЛллллллЛ ллЛ     
-echo      ллЩЭЭЭЭМллЩЭЭллЛллКллК     ллЩЭЭЭЭМллЩЭЭллЛллК     
-echo      лллллЛ  лллллллКллКллК     лллллЛ  ллК  ллКллК     
-echo      ллЩЭЭМ  ллЩЭЭллКллКллК     ллЩЭЭМ  ллК  ллКШЭМ     
-echo      ллК     ллК  ллКллКлллллллЛлллллллЛллллллЩМллЛ     
-echo      ШЭМ     ШЭМ  ШЭМШЭМШЭЭЭЭЭЭМШЭЭЭЭЭЭМШЭЭЭЭЭМ ШЭМ     
-echo.
+echo [31m"******************************************************"[0m
+echo [31m"*         _   _ ____  ____    _  _____ _____         *"[0m
+echo [31m"*        | | | |  _ \|  _ \  / \|_   _| ____|        *"[0m
+echo [31m"*        | | | | |_) | | | |/ _ \ | | |  _|          *"[0m
+echo [31m"*        | |_| |  __/| |_| / ___ \| | | |___         *"[0m
+echo [31m"*         \___/|_|   |____/_/  _\_\_|_|_____|        *"[0m
+echo [31m"*        |  ___/ \  |_ _| |   | ____|  _ \| |        *"[0m
+echo [31m"*        | |_ / _ \  | || |   |  _| | | | | |        *"[0m
+echo [31m"*        |  _/ ___ \ | || |___| |___| |_| |_|        *"[0m
+echo [31m"*        |_|/_/   \_\___|_____|_____|____/(_)        *"[0m
+echo [31m"*                                                    *"[0m
+echo [31m"******************************************************"[0m
 echo   You will need to fix the issues described in the 
 echo   log ^^^^ up there, or find someone who can!
 echo   Until the problem is fixed you will be running an
@@ -65,7 +66,6 @@ POPD
 exit /B 0
 
 :banner
-echo.
 echo [47m[30m"******************************************************"[0m
 echo [47m[30m"* [0m   ___  [47m[30m                                           *"[0m
 echo [47m[30m"* [0m  / __| [47m[30m __ _ __ __ ___  _ __   __ _  _ _          *"[0m
@@ -77,6 +77,5 @@ echo [47m[30m"*           / /   [0m | (__  [47m[30m/ _ \(_-<| '  \ / _ \(_-
 echo [47m[30m"*          /___|  [0m  \___| [47m[30m\___//__/|_|_|_|\___//__/  *"[0m
 echo [47m[30m"*                 [0m        [47m[30m                           *"[0m
 echo [47m[30m"******************************************************"[0m
-echo.
 
 exit /B 0
