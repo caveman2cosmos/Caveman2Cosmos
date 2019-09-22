11081baa3ed9afb6baaa5d84bdfa753a5e0bb65e
billw2012 <billw2011@gmail.com>
2019-09-22 16:33:43 +0100

    Autocorrect a lot faster, google calls reduced and guarded

--- Tools/Autocorrect/Autocorrect.py
+++ Tools/Autocorrect/Autocorrect.py
@@ -3,15 +3,21 @@ from lxml import etree
 # import xml.etree.ElementTree as ET
 import re
 import os
+import time
 import msvcrt
 import glob
+import random
 import requests
-from pylanguagetool import api
+# from pylanguagetool import api
 import logging
 import colorama
 from colorama import Fore, Back, Style
 from spellchecker import SpellChecker
 from googlesp import GoogleSp
+from random import randint
+from time import sleep
+from langtool import LanguageTool
+import urllib
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -36,7 +42,6 @@ def namespace(element):
     m = re.match(r'\{(.*)\}', element.tag)
     return m.group(1) if m else ''
 
-# define Python user-defined exceptions
 class ExitEarly(Exception):
    pass
 
@@ -45,34 +50,26 @@ def autocorrect(files, mode, args):
     ignore_tags = load_string_list('ignore_tag_list.txt')
     ignore_rules = load_string_list('ignore_rules_list.txt') + ['__ignore']
 
-    if args.pyspellchecker:
-        spell = SpellChecker()
-    else:
-        spell = None
-
-    if args.google:
-        google = GoogleSp()
-    else:
-        google = None
+    langtool = LanguageTool(api_url='http://localhost:8081/v2/', lang='en-US')
+    spell = SpellChecker(distance=1, local_dictionary=args.spell_dict)
+    google = GoogleSp()
 
     fancy = args.fancy
-    languagetool_sp = args.languagetool
 
     try:
         for filename in files:
             print(filename)
-            # text_file = ET.parse(filename)
+            indent = '  '
+            print(Fore.GREEN + indent + u'Searching', end='')
             root = etree.parse(filename).getroot()
-            #nspace = namespace(root)
             nsmap = {'': namespace(root)}
-            # .register_namespace('', nspace)
             try:
                 for text_elem in root.findall('TEXT', nsmap):
                     tag_elem = text_elem.find('Tag', nsmap)
                     if tag_elem is not None and tag_elem.text not in ignore_tags:
                         eng_elem = text_elem.find('English', nsmap)
                         if eng_elem is not None:
-                            autocorrect_element(eng_elem, tag_elem.text, ignore_words, ignore_tags, ignore_rules, mode, '  ', fancy, spell, google, languagetool_sp)
+                            autocorrect_element(eng_elem, tag_elem.text, ignore_words, ignore_tags, ignore_rules, mode, indent, fancy, langtool, spell, google)
             finally:
                 if mode != Mode.DETECT:
                     etree.ElementTree(root).write(filename, encoding="utf-8", xml_declaration=True, pretty_print=True)
@@ -97,45 +94,64 @@ def get_spellchecker_candidates(word, spell):
     # Correct capitalization
     if word == word.capitalize():
         candidates = set([c.capitalize() for c in candidates])
-    return candidates
+    return list(candidates)
 
-def apply_spellcheck(matches, ignore_words, spell, google, languagetool_sp):
+last_googled = 0
+
+def print_progress(color = Style.RESET_ALL):
+    if color == 0:
+        print(random.choice([Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW]) +'.' + Style.RESET_ALL, end='')
+    else:
+        print(color +'.' + Style.RESET_ALL, end='')
+
+def apply_spellcheck(matches, ignore_words, spell, google):
+    global last_googled
     for match in [m for m in matches if m['rule']['id'] in spell_check_replacement_rules]:
         match['rule']['id'] = 'spellcheck_replacement'
         context = match['context']
         text = context['text']
         word = text[context['offset']:context['offset']+context['length']]
         if word in ignore_words:
-            # ignore this as the word is in our ignore list (shouldn't be here if it is but whatevs)
             match['rule']['id'] = '__ignore'
             continue
 
-        candidates = []
-        # prefer google
-        if google:
+        print_progress(Fore.YELLOW)
+        candidates = get_spellchecker_candidates(word, spell)
+
+        if not candidates or len(candidates) == 0:
+            match['rule']['id'] = '__ignore'
+            continue
+        try:
+            # don't spam google too hard...
+            sleep(max(0.01, 1 - (time.time() - last_googled)))
+            print_progress(Fore.RED)
             google_candidate = google.correct(word)
-            if google_candidate and google_candidate != word:
-                candidates = candidates + [google_candidate]
-        # pyspellchecker second
-        if spell:
-            candidates = candidates + get_spellchecker_candidates(word, spell)
-        # languagetool suggestions last
-        if languagetool_sp:
-            candidates = candidates + [r['value'] for r in match['replacements']]
+            last_googled = time.time()
+            if not google_candidate or google_candidate == word:
+                match['rule']['id'] = '__ignore'
+                continue
+        except urllib.error.HTTPError as ex:
+            print('**** ERROR: google spell check request failed: ' + ex.reason)
+            print('**** Falling back to default spellcheck')
+
+        candidates = candidates + [r['value'] for r in match['replacements']]
 
         if len(candidates) == 0:
-            # ignore this as it isn't a misspelling according to spellcheck dictionary
             match['rule']['id'] = '__ignore'
         else:
             match['replacements'] = [{'value': v} for v in set(candidates)]
 
-def autocorrect_element(eng_elem, tag, ignore_words, ignore_tags, ignore_rules, mode, indent, fancy, spell, google, languagetool_sp):
-    results = api.check(eng_elem.text,
-                        api_url='http://localhost:8081/v2/', lang='en-US', disabled_rules=ignore_rules, pwl=ignore_words)
+def autocorrect_element(eng_elem, tag, ignore_words, ignore_tags, ignore_rules, mode, indent, fancy, langtool, spell, google):
+    print_progress(Fore.BLUE)
+    #start = time.time()
+    results = langtool.check(eng_elem.text) # , disabled_rules=ignore_rules, pwl=ignore_words
+    # print('lt: ' + str(time.time() - start))
     if results and 'matches' in results and len(results['matches']) > 0:
-        apply_spellcheck(results['matches'], ignore_words, spell, google, languagetool_sp)
+        apply_spellcheck(results['matches'], ignore_words, spell, google)
+
         matches = [r for r in results['matches'] if r['rule']['id'] not in ignore_rules]
         if len(matches) > 0:
+            print()
             # Make sure its sorted by offset (it should be already)
             matches.sort(key=lambda x: x['offset'])
 
@@ -151,14 +167,11 @@ def autocorrect_element(eng_elem, tag, ignore_words, ignore_tags, ignore_rules, 
                 print(Fore.GREEN + indent + u'Suggested text:')
                 print(Fore.WHITE + indent + '    ' + corrected_text_with_markers)
 
-            indent2 = indent + '    '
-
             if mode == Mode.INTERACTIVE:
                 if eng_elem.text != corrected_text:
                     print(Fore.BLUE + indent + '> Accept all (return), Skip (s), Interactive (space), Ignore ' + Fore.CYAN + tag + Fore.BLUE + ' (x), Exit (esc)?', end = '')
                 else:
                     print(Fore.BLUE + indent + '> Skip (s), Interactive (space), Ignore ' + Fore.CYAN + tag + Fore.BLUE + ' (x), Exit (esc)?', end = '')
-
                 key = msvcrt.getch()
                 print('')
                 if key == b'\r':
@@ -170,20 +183,18 @@ def autocorrect_element(eng_elem, tag, ignore_words, ignore_tags, ignore_rules, 
                         eng_elem.text, matches, ignore_words, ignore_rules, indent + '    ', fancy)
                     print(Fore.GREEN + indent + u'Corrected text:')
                     print(Fore.WHITE + indent + '    ' + corrected_text_with_markers)
-                    #print(Fore.GREEN + indent2 + u'Corrected text: ' + Fore.WHITE + corrected_text_with_markers)
-                    
                     eng_elem.text = corrected_text
                 elif key == b'x':
                     ignore_tags.append(tag)
                     print(Fore.GREEN + indent + 'Added ' + Fore.CYAN + tag + Fore.GREEN + ' to the global ignore list')
-                elif key == b's':
-                    print(Fore.GREEN + indent + 'Skipping ' + Fore.CYAN + tag)
-                else:
+                elif key == b'\x1b':
                     print(Fore.GREEN + indent + 'Exiting...')
                     raise ExitEarly
+                else: #key == b's':
+                    print(Fore.GREEN + indent + 'Skipping ' + Fore.CYAN + tag)
             elif mode == Mode.AUTOMATIC:
                 eng_elem.text = corrected_text
-            print('\n' + Fore.GREEN + indent + u'Continuing search ...\n')
+            print('\n' + Fore.GREEN + indent + u'Continuing search', end='')
 
 def apply_corrections(text, matches, fancy):
     # We need to keep track of changes introduced by corrections to
@@ -206,10 +217,7 @@ def apply_corrections(text, matches, fancy):
     offset_adjust_orig = 0
     orig_with_markers = text
     for match in matches:
-        # print('    %s: %s' % (match['message'], match['context']['text']))
         if 'replacements' in match and len(match['replacements']) > 0:
-            # print('    - Suggestions: %s' %
-            #     ', '.join([v['value'] for v in match['replacements']]))
             replacement = match['replacements'][0]['value']
 
             offset = match['offset'] + offset_adjust
@@ -234,7 +242,6 @@ def create_error_underlines(context):
     return ' ' * offset + '^' * length
 
 def replace(replacement, match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks, fancy):
-    # nonlocal offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks
     if fancy:
         start_marker_corrected = Fore.GREEN
         end_marker_corrected = Style.RESET_ALL
@@ -296,17 +303,7 @@ def apply_corrections_interactive(text, matches, ignore_words, ignore_rules, ind
                     index = ord(key) - ord(b'0')
                 replacement = match['replacements'][index]['value']
                 print(Fore.GREEN + indent + 'Applied suggestion ' + Fore.WHITE + replacement)
-                # offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(match['replacements'][index]['value'], match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks)
                 offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(replacement, match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks, fancy)
-                # replacement = match['replacements'][index]['value']
-                # orig_text_len = len(corrected_text)
-                # corrected_text = corrected_text[:offset] + replacement + corrected_text[offset+length:]
-                # offs_adj = offs_adj + (len(corrected_text) - orig_text_len)
-
-                # offset = match['offset'] + offs_adj_mrks
-                # orig_text_len = len(corrected_text_mrks)
-                # corrected_text_mrks = corrected_text_mrks[:offset] + replacement + corrected_text_mrks[offset+length:]
-                # offs_adj_mrks = offs_adj_mrks + (len(corrected_text_mrks) - orig_text_len)
             elif can_fix and key == b'a':
                 print(Fore.GREEN + indent + 'Added word ' + Fore.RED + to_replace + Fore.GREEN + ' to the global ignore list')
                 ignore_words.append(to_replace)
@@ -314,15 +311,14 @@ def apply_corrections_interactive(text, matches, ignore_words, ignore_rules, ind
                 print(Fore.GREEN + indent + 'Added rule ' + Fore.WHITE + match['rule']['id'] + Fore.GREEN + ' to the global ignore list')
                 ignore_rules.append(match['rule']['id'])
             elif can_fix and key == b'c':
-                #offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(input('Enter text to replace %s > ' % to_replace), match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks)
                 print(Fore.BLUE + indent + 'Enter text to replace ' + Fore.RED + to_replace + Fore.BLUE + ' > ' + Fore.WHITE, end = '')
                 offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks = replace(input(), match, offs_adj, corrected_text, offs_adj_mrks, corrected_text_mrks, fancy)
-            elif key == b's':
-                print(Fore.GREEN + indent + 'Skipping this suggestion')
-                pass
-            else:
+            elif key == b'\x1b':
                 print(Fore.GREEN + indent + 'Exiting...')
                 raise ExitEarly
+            else: # key == b's':
+                print(Fore.GREEN + indent + 'Skipping this suggestion')
+                pass
 
     return corrected_text, corrected_text_mrks
 
@@ -331,19 +327,12 @@ if __name__ == "__main__":
 
     print(Style.BRIGHT + Fore.BLUE + 'C2C Autocorrect' + Style.RESET_ALL + ' v0.1 by billw\n')
 
-    parser = argparse.ArgumentParser(
-        description='Detect grammar and spelling mistakes in an XML file using Language Tool 4.6, and optionally correct them.')
+    parser = argparse.ArgumentParser(description='Detect grammar and spelling mistakes in an XML file using Language Tool 4.6, and optionally correct them.')
     parser.add_argument('glob_patterns', metavar='glob', type=str,
                         nargs='+', help='globs describing what files to process (e.g. "*_CIV4GameText.xml")')
     parser.add_argument('--log', dest='log_file', action='store', help='override log file name', default='Autocorrect.log')
     parser.add_argument('--non-fancy', dest='fancy', action='store_false',
                             help="don't use colors for markup", default=True)
-    parser.add_argument('--google', dest='google', action='store_true',
-                            help="use google for spell check suggestion", default=False)
-    parser.add_argument('--pyspellchecker', dest='pyspellchecker', action='store_true',
-                            help="use pyspellchecker for spell check suggestions", default=False)
-    parser.add_argument('--languagetool', dest='languagetool', action='store_true',
-                            help="use LanguageTool for spell check suggestions", default=False)
     parser.add_argument('--dict', dest='spell_dict', help="use specified spellcheck dictionary", default='dicts\\en_full.json')
     mode_group = parser.add_mutually_exclusive_group(required=False)
     mode_group.add_argument('--automatic', dest='automatic', action='store_true',
@@ -372,7 +361,9 @@ if __name__ == "__main__":
     if len(unique_files) == 0:
         print(Fore.YELLOW + 'No files found matching the provided blobs!')
     else:
-        print('Processing %d files...' % len(unique_files))
+        if len(unique_files) > 1:
+            print('Processing %d files...' % len(unique_files))
+
         try:
             autocorrect(unique_files, mode, args)
 
