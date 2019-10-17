d9a5065ab08dee3523bf85b9d2172f903b8d1a01
billw2012 <billw2011@gmail.com>
2019-10-17 13:51:44 +0100

    CyArgsList supports fluent like construction syntax

--- Sources/CyArgsList.h
+++ Sources/CyArgsList.h
@@ -3,42 +3,58 @@
 #ifndef CyArgsList_h
 #define CyArgsList_h
 
+template < class Ty_ >
+class CyArrayArg
+{
+	typedef Ty_ value_type;
+	
+	CyArrayArg(const value_type* vals, int len) : vals(vals), len(len) {}
+
+	const value_type* vals;
+	int len;
+};
 //
 // type for input args to python functions
 //
 class CyArgsList
 {
 public:
-	enum
-	{
-		MAX_CY_ARGS=20
-	};
-	CyArgsList() : m_iCnt(0), m_aList() {}
+	CyArgsList() : m_nArgs(0) {}
+
+	// Exe/Dll interface (DO NOT CHANGE)
 	DllExport void add(int i);
-	void add(uint ui) { add((int)ui);	}
 	DllExport void add(float f);
 	DllExport void add(const char* s);					// null-terminated string
 	DllExport void add(const wchar* s);					// null-terminated widestring
 	DllExport void add(const char* s, int iLength);		// makes a data string
+	DllExport void add(void* p);
+	DllExport void* makeFunctionArgs();
+
+	// Extended interface
+	void add(uint ui) { add((int)ui); }
 	void add(const byte* s, int iLength);		// makes a list
 	void add(const int* s, int iLength);		// makes a list
 	void add(const float* s, int iLength);		// makes a list
-	DllExport void add(void* p);
-	DllExport void* makeFunctionArgs();
-	int size() const { return m_iCnt;	}
-	void push_back(void* p) { FAssertMsg(m_iCnt<MAX_CY_ARGS, "increase cyArgsList::MAX_CY_ARGS"); m_aList[m_iCnt++] = p; }
-	void clear() { m_iCnt=0;	}
-	void* m_aList[MAX_CY_ARGS];
+	template < class Ty_ >
+	void add(const CyArrayArg<Ty_>& arr) { add(arr.vals, arr.len); }
+
+	int size() const { return m_nArgs; }
+	void clear() { m_nArgs = 0; }
 
 	template < class Ty_ >
-	CyArgsList& operator<<(const Ty_& arg)
+	CyArgsList& operator<<(const Ty_& arg) { add(arg); return *this; }
+
+private:
+	static const int MAX_CY_ARGS = 20;
+
+	void push_back(void* p) 
 	{
-		add(arg);
-		return *this;
+		FAssertMsg(m_nArgs + 1 < MAX_CY_ARGS, "Exceeded maximum allowed number of Python arguments for a function");
+		m_aList[m_nArgs++] = p;
 	}
 
-protected:
-	int m_iCnt;
+	void* m_aList[MAX_CY_ARGS];
+	int m_nArgs;
 };
 
 #endif	// CyArgsList_h
