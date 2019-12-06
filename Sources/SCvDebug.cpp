523dbcced1d53f3edb86a6a48b0d969a570b45de
billw2012 <billw2011@gmail.com>
2019-12-06 13:22:57 +0000

    Fix operator precedence for bit mask

--- Sources/SCvDebug.cpp
+++ Sources/SCvDebug.cpp
@@ -5,7 +5,7 @@ unsigned __int64 SCvDebug::m_mode = 0;
 bool SCvDebug::checkMode(Mode mode)
 {
 	FAssert(mode >= 0 && mode < 64);
-	return m_mode & (1 << mode) != 0;
+	return (m_mode & (1 << mode)) != 0;
 }
 
 void SCvDebug::setMode(Mode mode, bool on)
