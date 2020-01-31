46a7e91c0b1d9153cb02c1999be6393091ec425d
Matt <qqwweeaassddzzzxxxccc111@gmail.com>
2020-01-31 09:31:16 -0500

    exposed functions to python

--- Sources/CyPlayer.h
+++ Sources/CyPlayer.h
@@ -25,6 +25,8 @@ public:
 	CvPlayer* getPlayer() { return m_pPlayer;	}	// Call from C++
 	bool isNone() { return (m_pPlayer==NULL); }
 
+	void updateMembers();
+	void initMembers(const int iIndex);
 /************************************************************************************************/
 /* CHANGE_PLAYER                         08/27/08                                 jdog5000      */
 /*                                                                                              */
