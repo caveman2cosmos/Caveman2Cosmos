2ba83574075cb209e418bd9c30f2d2ff00f70ae8
Matt <qqwweeaassddzzzxxxccc111@gmail.com>
2020-04-12 11:11:34 -0400

    removed functions from CyMapGenerator that arn't called from python.

--- Sources/CyMapGenerator.h
+++ Sources/CyMapGenerator.h
@@ -21,34 +21,14 @@ class CyMapGenerator
 public:
 	CyMapGenerator();
 	CyMapGenerator(CvMapGenerator* pMapGenerator);		// Call from C++
-	CvMapGenerator* getMapGenerator() { return m_pMapGenerator;	}	// Call from C++
 	bool isNone() { return (m_pMapGenerator==NULL); }
 
-	bool canPlaceBonusAt(int /*BonusTypes*/ eBonus, int iX, int iY, bool bIgnoreLatitude);
-	bool canPlaceGoodyAt(int /*ImprovementTypes*/ eImprovement, int iX, int iY);
-
-	void addGameElements();
-
-	void addLakes();
-	void addRivers();
 	void doRiver(CyPlot* pStartPlot, CardinalDirectionTypes eCardinalDirection);
 	void addFeatures();
 	void addBonuses();
-	void addUniqueBonusType(int /*BonusTypes*/ eBonusType);
-	void addNonUniqueBonusType(int /*BonusTypes*/ eBonusType);
-	void addGoodies();
-
-	void eraseRivers();
-	void eraseFeatures();
-	void eraseBonuses();
-	void eraseGoodies();
-
-	void generateRandomMap();
 
 	void generatePlotTypes();
 	void generateTerrain();
-
-	void afterGeneration();
 
 	void setPlotTypes(boost::python::list& listPlotTypes);
 
