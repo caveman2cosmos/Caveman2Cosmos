5fb877ef0386c6a6c3d3754890aefdc65c178ccc
Matt <qqwweeaassddzzzxxxccc111@gmail.com>
2020-04-18 17:41:48 -0400

    Revert "Added back the FAssert to the getInfoTypeForString calls that come from python."
    This reverts commit 51181302a087407d0419c9939d4fdd2bd6ac67fe.

--- Sources/CyGlobalContext.cpp
+++ Sources/CyGlobalContext.cpp
@@ -462,7 +462,7 @@ CvPropertyInfo* CyGlobalContext::getPropertyInfo(int i) const
 
 int CyGlobalContext::getInfoTypeForString(const char* szInfoType) const
 {
-	return GC.getInfoTypeForString(szInfoType);
+	return GC.getInfoTypeForString(szInfoType, true);
 }
 /************************************************************************************************/
 /* Afforess	                  Start		 03/18/10                                               */
