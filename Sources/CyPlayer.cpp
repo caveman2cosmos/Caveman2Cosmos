cc0e8109450fa16cb2ad2c6c8e0d352184cf39bb
Toffer <toffer_krix-offer@hotmail.com>
2020-10-30 18:50:32 +0100

    Minor thing I forgot in last commit.

--- Sources/CyPlayer.cpp
+++ Sources/CyPlayer.cpp
@@ -1904,18 +1904,7 @@ bool CyPlayer::isFeatAccomplished(int /*FeatTypes*/ eIndex)
 
 void CyPlayer::setFeatAccomplished(int /*FeatTypes*/ eIndex, bool bNewValue)
 {
-	//TB Something's wrong in the python and this allows me to protect against it.
-	if (eIndex < 0)
-	{
-		FErrorMsg("eIndex is expected to be non-negative (invalid Feat Called by Python and Caught Early)");
-		return;
-	}
-	if(eIndex >= NUM_FEAT_TYPES)
-	{
-		FErrorMsg("eIndex is expected to be within maximum bounds (invalid Feat Called by Python and Caught Early)");
-		return;
-	}
-	else if (m_pPlayer)
+	if (m_pPlayer)
 	{
 		m_pPlayer->setFeatAccomplished((FeatTypes)eIndex, bNewValue);
 	}
