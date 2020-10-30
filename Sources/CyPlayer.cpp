f1d3655b5da6877c569398bb372fb097ff2f74c6
Toffer <toffer_krix-offer@hotmail.com>
2020-10-30 18:48:05 +0100

    revised the feat coding a bit and brought back the you've trained your first naval unit feat that raxo recently removed.

--- Sources/CyPlayer.cpp
+++ Sources/CyPlayer.cpp
@@ -1899,17 +1899,6 @@ int CyPlayer::getGoldPerTurnByPlayer(int /*PlayerTypes*/ eIndex)
 
 bool CyPlayer::isFeatAccomplished(int /*FeatTypes*/ eIndex)	
 {
-	//TB Something's wrong in the python and this allows me to protect against it.
-	if (eIndex < 0)
-	{
-		FErrorMsg("eIndex is expected to be non-negative (invalid Feat Called by Python and Caught Early)");
-		return false;
-	}
-	if (eIndex >= NUM_FEAT_TYPES)
-	{
-		FErrorMsg("eIndex is expected to be within maximum bounds (invalid Feat Called by Python and Caught Early)");
-		return false;
-	}
 	return m_pPlayer ? m_pPlayer->isFeatAccomplished((FeatTypes)eIndex) : false;
 }
 
