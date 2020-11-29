d94b2ddf7c5906b1af5d22755002a9fab8f7943b
Toffer <toffer_krix-offer@hotmail.com>
2020-11-29 22:09:04 +0100

    More testing

--- Tools/_MakeDLL.bat
+++ Tools/_MakeDLL.bat
@@ -9,6 +9,11 @@ POPD
 REM Switch to the source directory
 PUSHD "%tools_dir%\..\Sources"
 
+if "%APPVEYOR_REPO_BRANCH%" eq "%release_branch%" (
+    echo Skipping test build due to being on release branch
+    exit /b 0
+)
+
 call Powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%tools_dir%\_Build.ps1" %*
 if %ERRORLEVEL% NEQ 0 (
     goto :exit_failed
