93d5d34dd51352088b4d9184ce3b596df10a2edf
DZF <dzfischer@gmail.com>
2020-12-16 14:23:14 -0500

    wip bonus discover/deplete tool

---
+++ Tools/XMLTools/BlazeScripts/BonusesByImprovementsListing.py
@@ -0,0 +1,76 @@
+import csv
+import lxml.etree as ET
+
+
+path_xml_folder =  '../../../Assets/XML'
+
+
+path_improvements = "/Terrain/CIV4ImprovementInfos.xml"
+
+#####################################################
+
+# Given path to xml file, return (root, schema) of that file
+def load_tree(path_to_xml):
+    tree = ET.parse(path_to_xml)
+    root = tree.getroot()
+    tag = root.tag
+    schema = tag[:tag.find('}')+1] #probably some .attrib way to get it but this works too
+    return tree, root, schema
+
+# helper functions to slightly reduce retyping
+def find_text(element, schema, child_element, return_if_missing = None):
+    tag = element.find(f"{schema}{child_element}")
+    if tag != None:
+        text = element.find(f"{schema}{child_element}").text
+        return text
+    else:
+        return return_if_missing
+
+
+"""
+psuedocode:
+b_dict = {bonus: [iDiscoverRand1, iDiscoverRand2, ...]} where position of iDiscoverRand values correlates to list of improvements
+for improvement
+    if has bonus structs entry
+        for bonus struct
+            add improvement to array
+
+for k, v in dict
+    print k fancy
+    for v2 in dict
+        print v2 fancy
+"""
+
+# It would be much simpler to pre-alloc an overlarge array to fill then later prune or even dynamically resize one, but owell
+imp_list = []
+b_dict = {}
+_, root, schema = load_tree(path_xml_folder+path_improvements)
+for imp_info in root[0].findall(f"{schema}ImprovementInfo"):
+    imp = find_text(imp_info, schema, 'Type')
+    b_structs = imp_info.find(f"{schema}BonusTypeStructs")
+    if b_structs != None:
+        if imp not in imp_list:
+            imp_list.append(imp)
+        for b_struct in b_structs.findall(f"{schema}BonusTypeStruct"):
+            b_name = find_text(b_struct, schema, 'BonusType')
+            b_disc = find_text(b_struct, schema, 'iDiscoverRand', 0)
+            if b_dict.get(b_name) == None:
+                b_dict[b_name] = [0]*len(imp_list)
+                b_dict[b_name][imp_list.index(imp)] = b_disc
+            else:
+                if len(b_dict[b_name]) < len(imp_list):
+                    b_dict[b_name] += [0]*(len(imp_list) - len(b_dict[b_name]))
+                b_dict[b_name][imp_list.index(imp)] = b_disc
+
+# print(['Bonuses']+imp_list)
+# exit()
+
+with open ('Reports/BonusBreakdown.csv', 'w', newline='') as csvf:
+    csvw = csv.writer(csvf)
+    csvw.writerow(['Bonuses'] + imp_list)
+    for b_name, chances in b_dict.items():
+        # extending 0s
+        if len(chances) < len(imp_list):
+            chances += [0]*(len(imp_list) - len(chances))
+
+        csvw.writerow([b_name]+chances)
