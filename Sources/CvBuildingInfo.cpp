e9dcf6dd2260684f78f7e09756b08cb9f8bce1a8
MattOttawa <58055416+MattOttawa@users.noreply.github.com>
2020-12-17 18:59:36 -0500

    AI code for BoolExpr by AIAndy (#783)

--- Sources/CvBuildingInfo.cpp
+++ Sources/CvBuildingInfo.cpp
@@ -1896,12 +1896,30 @@ int CvBuildingInfo::getMaxCultureLevelAllowed() const
 
 bool CvBuildingInfo::EnablesOtherBuildings() const
 {
-	if ( !m_bEnablesOtherBuildingsCalculated )
+	if (!m_bEnablesOtherBuildingsCalculated)
 	{
 		const int iId = GC.getInfoTypeForString(getType());
 
 		m_bEnablesOtherBuildingsCalculated = true;
 		m_bEnablesOtherBuildingsValue = false;
+
+		// add the building and its bonuses to the query to see if they influence the construct condition of a building
+		std::vector<GOMQuery> queries;
+		GOMQuery query;
+		query.GOM = GOM_BUILDING;
+		query.id = iId;
+		queries.push_back(query);
+		query.GOM = GOM_BONUS;
+		query.id = getFreeBonus();
+		if (query.id != NO_BONUS)
+		{
+			queries.push_back(query);
+		}
+		for (int iJ = 0; iJ < getNumExtraFreeBonuses(); iJ++)
+		{
+			query.id = getExtraFreeBonus(iJ);
+			queries.push_back(query);
+		}
 
 		for (int iJ = 0; iJ < GC.getNumBuildingInfos(); iJ++)
 		{
@@ -1970,9 +1988,37 @@ bool CvBuildingInfo::EnablesUnits() const
 		m_bEnablesUnitsCalculated = true;
 		m_bEnablesUnits = false;
 
+		// add the building and its bonuses to the query to see if they influence the construct condition of a building
+		std::vector<GOMQuery> queries;
+		GOMQuery query;
+		query.GOM = GOM_BUILDING;
+		query.id = eBuilding;
+		queries.push_back(query);
+		query.GOM = GOM_BONUS;
+		query.id = getFreeBonus();
+		if (query.id != NO_BONUS)
+		{
+			queries.push_back(query);
+		}
+		for (int iJ = 0; iJ < getNumExtraFreeBonuses(); iJ++)
+		{
+			query.id = getExtraFreeBonus(iJ);
+			queries.push_back(query);
+		}
+
 		for (int iI = 0; iI < GC.getNumUnitInfos(); iI++)
 		{
 			const CvUnitInfo& kUnit = GC.getUnitInfo((UnitTypes)iI);
+
+			BoolExpr* condition = kUnit.getTrainCondition();
+			if (condition != NULL)
+			{
+				if (condition->getInvolvesGOM(&(*queries.begin()), &(*queries.end())))
+				{
+					m_bEnablesUnits = true;
+					return m_bEnablesUnits;
+				}
+			}
 
 			if (kUnit.isPrereqAndBuilding(eBuilding))
 			{
