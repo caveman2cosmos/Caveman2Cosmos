5b8be1656d087827035a8fec9f8719dfc4d4da43
MattOttawa <qqwweeaassddzzzxxxccc111@gmail.com>
2021-05-02 16:33:24 -0400

    clean-up

--- PrivateMaps/C2C_Donut.py
+++ PrivateMaps/C2C_Donut.py
@@ -259,7 +259,7 @@ def generateTerrainTypes():
 class DonutFeatureGenerator(CvMapGeneratorUtil.FeatureGenerator):
 	def addIceAtPlot(self, pPlot, iX, iY, lat):
 		# We don' need no steeking ice. M'kay? Alrighty then.
-		ice = 0
+		return
 		
 	def addJunglesAtPlot(self, pPlot, iX, iY, lat):
 		if (CyMap().getCustomMapOption(1) == 1): pass #No Jungles option
