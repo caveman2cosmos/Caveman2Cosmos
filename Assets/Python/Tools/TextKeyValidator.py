7d2532d7c459fc0cad7e1c09bc0979199783dd37
MattOttawa <qqwweeaassddzzzxxxccc111@gmail.com>
2021-05-10 05:19:20 -0400

    update

--- Assets/Python/Tools/TextKeyValidator.py
+++ Assets/Python/Tools/TextKeyValidator.py
@@ -1,22 +1,13 @@
 from os.path import join
 from XmlUtil import getTagValues, getGlob
-import SystemPaths as SP
 
 def main():
 	xmlDir = getXmlDir()
-	dirCiv4 = SP.dirBtS[:len(SP.dirBtS) - len("\Beyond the Sword")]
-	files  = getGlob(join(xmlDir, "Text"))
-	files += getGlob(join(SP.dirBtS, "Assets", "XML", "Text"))
-	files += getGlob(join(dirCiv4, "Assets", "XML", "Text"))
-	files += getGlob(join(dirCiv4, "Warlords", "Assets", "XML", "Text"))
-	existingKeys = getTagValues(("Tag",), files)
+	existingKeys = getTagValues(("Tag",), getGlob(join(xmlDir, "GameText")))
 	referencedKeys = getTagValues(("Description", "Help", "Civilopedia", "Strategy", "Quote"), getGlob(xmlDir))
 	print "Validating text keys"
-	validate(referencedKeys, existingKeys)
-
-def validate(referencedKeys, existingKeys):
 	for key in referencedKeys:
-		if not key in existingKeys:
+		if key.startswith("TXT_KEY_") and not key in existingKeys:
 			print "Referenced key not found: %s" %key
 
 def getXmlDir():
