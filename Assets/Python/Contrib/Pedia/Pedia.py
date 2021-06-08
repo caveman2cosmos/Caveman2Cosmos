37b4d321d1ef7ad06a821cb456db0621c7294c0b
raxo2222 <raxo8888@gmail.com>
2021-06-08 22:24:59 +0200

    It produced results for other types too

--- Assets/Python/Contrib/Pedia/Pedia.py
+++ Assets/Python/Contrib/Pedia/Pedia.py
@@ -1207,6 +1207,7 @@ class Pedia:
 					print CvBuildingInfo.getType()+" Tech obsolete: "+str(GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX())+" Health Changes late tech: "+str(iTechTLoc)+" "+GC.getTechInfo(iTech).getType()
 			"""
 			
+			#Check if Yield Changes techs don't appear before building can be unlocked or after is obsoleted
 			if CvBuildingInfo.isAnyTechYieldChanges():
 				iTechMod = 0
 				while iTechMod < GC.getNumTechInfos():
@@ -1218,6 +1219,66 @@ class Pedia:
 								print CvBuildingInfo.getType()+" Tech unlock: "+str(iTechLoc)+" Yield Changes early tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
 							elif CvBuildingInfo.getObsoleteTech() != -1 and iTechMLoc >= GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX():
 								print CvBuildingInfo.getType()+" Tech obsolete: "+str(GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX())+" Yield Changes late tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
+						i += 1
+					iTechMod += 1
+					
+			#Check if Yield Modifiers techs don't appear before building can be unlocked or after is obsoleted
+			if CvBuildingInfo.isAnyTechYieldModifiers():
+				iTechMod = 0
+				while iTechMod < GC.getNumTechInfos():
+					i = 0
+					while i < YieldTypes.NUM_YIELD_TYPES:
+						if CvBuildingInfo.getTechYieldModifier(iTechMod, i):
+							iTechMLoc = GC.getTechInfo(iTechMod).getGridX()
+							if GC.getTechInfo(iTechMod) != None and iTechMLoc <= iTechLoc:
+								print CvBuildingInfo.getType()+" Tech unlock: "+str(iTechLoc)+" Yield Modifiers early tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
+							elif CvBuildingInfo.getObsoleteTech() != -1 and iTechMLoc >= GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX():
+								print CvBuildingInfo.getType()+" Tech obsolete: "+str(GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX())+" Yield Modifiers late tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
+						i += 1
+					iTechMod += 1
+					
+			#Check if Commerce Changes techs don't appear before building can be unlocked or after is obsoleted
+			if CvBuildingInfo.isAnyTechCommerceChanges():
+				iTechMod = 0
+				while iTechMod < GC.getNumTechInfos():
+					i = 0
+					while i < CommerceTypes.NUM_COMMERCE_TYPES:
+						if CvBuildingInfo.getTechCommerceChange(iTechMod, i):
+							iTechMLoc = GC.getTechInfo(iTechMod).getGridX()
+							if GC.getTechInfo(iTechMod) != None and iTechMLoc <= iTechLoc:
+								print CvBuildingInfo.getType()+" Tech unlock: "+str(iTechLoc)+" Commerce Changes early tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
+							elif CvBuildingInfo.getObsoleteTech() != -1 and iTechMLoc >= GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX():
+								print CvBuildingInfo.getType()+" Tech obsolete: "+str(GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX())+" Commerce Changes late tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
+						i += 1
+					iTechMod += 1
+			
+			#Check if Commerce Modifiers techs don't appear before building can be unlocked or after is obsoleted
+			if CvBuildingInfo.isAnyTechCommerceModifiers():
+				iTechMod = 0
+				while iTechMod < GC.getNumTechInfos():
+					i = 0
+					while i < CommerceTypes.NUM_COMMERCE_TYPES:
+						if CvBuildingInfo.getTechCommerceModifier(iTechMod, i):
+							iTechMLoc = GC.getTechInfo(iTechMod).getGridX()
+							if GC.getTechInfo(iTechMod) != None and iTechMLoc <= iTechLoc:
+								print CvBuildingInfo.getType()+" Tech unlock: "+str(iTechLoc)+" Commerce Modifiers early tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
+							elif CvBuildingInfo.getObsoleteTech() != -1 and iTechMLoc >= GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX():
+								print CvBuildingInfo.getType()+" Tech obsolete: "+str(GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX())+" Commerce Modifiers late tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
+						i += 1
+					iTechMod += 1
+					
+			#Check if Specialist Changes techs don't appear before building can be unlocked or after is obsoleted
+			if CvBuildingInfo.isAnyTechSpecialistChanges():
+				iTechMod = 0
+				while iTechMod < GC.getNumTechInfos():
+					i = 0
+					while i < GC.getNumSpecialistInfos():
+						if CvBuildingInfo.getTechSpecialistChange(iTechMod, i):
+							iTechMLoc = GC.getTechInfo(iTechMod).getGridX()
+							if GC.getTechInfo(iTechMod) != None and iTechMLoc <= iTechLoc:
+								print CvBuildingInfo.getType()+" Tech unlock: "+str(iTechLoc)+" Specialist Changes early tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
+							elif CvBuildingInfo.getObsoleteTech() != -1 and iTechMLoc >= GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX():
+								print CvBuildingInfo.getType()+" Tech obsolete: "+str(GC.getTechInfo(CvBuildingInfo.getObsoleteTech()).getGridX())+" Specialist Changes late tech: "+str(iTechMLoc)+" "+GC.getTechInfo(iTechMod).getType()
 						i += 1
 					iTechMod += 1
 
