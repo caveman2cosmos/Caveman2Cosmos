39443ed611adead8d4b4fd95eb1c18586b5b76ea
MattOttawa <qqwweeaassddzzzxxxccc111@gmail.com>
2021-07-01 14:27:20 -0400

    merged CvPropertySolver and CvMainPropertySolver

--- Sources/CvPropertySolver.h
+++ Sources/CvPropertySolver.h
@@ -90,11 +90,11 @@ protected:
 	std::vector<int> m_aiCurrentAmount;
 };
 
-class CvMainPropertySolver;
-
 class CvPropertySolver
 {
 public:
+	void doTurn();
+
 	void instantiateSource(const CvGameObject* pObject, CvPropertySource* pSource);
 	void instantiateInteraction(const CvGameObject* pObject, CvPropertyInteraction* pInteraction);
 	void instantiatePropagator(const CvGameObject* pObject, CvPropertyPropagator* pPropagator);
@@ -104,17 +104,12 @@ public:
 	
 	void predictSources();
 	void correctSources();
-	void clearSources();
 	
 	void predictInteractions();
 	void correctInteractions();
-	void clearInteractions();
 
 	void predictPropagators();
 	void correctPropagators();
-	void clearPropagators();
-
-	void setMainSolver(CvMainPropertySolver* pMainSolver);
 
 	std::vector<int>& getCache1();
 	std::vector<int>& getCache2();
@@ -124,37 +119,18 @@ public:
 	int getPredictValue(const CvGameObject* pObject, PropertyTypes eProperty) const;
 
 protected:
+	void gatherGlobalManipulators();
+	void resetPropertyChanges();
+	void gatherAndSolve();
+
 	std::vector<PropertySourceContext> m_aSourceContexts;
 	std::vector<PropertyInteractionContext> m_aInteractionContexts;
 	std::vector<PropertyPropagatorContext> m_aPropagatorContexts;
-	CvMainPropertySolver* m_pMainSolver;
 	std::vector<int> m_aiCache1;
 	std::vector<int> m_aiCache2;
-};
 
-class CvMainPropertySolver
-{
-public:
-	CvMainPropertySolver();
-
-	PropertySolverMap* getSolverMap();
-
-	void addGlobalManipulators(const CvPropertyManipulators* pMani);
-	void gatherGlobalManipulators();
-	int getNumGlobalManipulators() const;
-	const CvPropertyManipulators* getGlobalManipulator(int index) const;
-
-	void resetPropertyChanges();
-
-	void gatherAndSolve();
-
-	void doTurn();
-
-protected:
 	std::vector<const CvPropertyManipulators*> m_apGlobalManipulators;
-	CvPropertySolver m_Solver;
 	PropertySolverMap m_mapProperties;
 };
-
 
 #endif
