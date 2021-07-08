d82aad40f59f067a067064310e4c700671f25545
raxo2222 <raxo8888@gmail.com>
2021-07-08 10:09:25 +0200

    Expanded GOM-PY interface to cover units, OR type requirements, and buildings/techs

--- Sources/CyBoolExprInterface.cpp
+++ Sources/CyBoolExprInterface.cpp
@@ -5,13 +5,16 @@ void CyBoolExprPythonInterface()
 {
 	python::enum_<GOMTypes>("GOMTypes")
 		.value("NO_GOM", NO_GOM)
+		.value("GOM_BUILDING", GOM_BUILDING)
 		.value("GOM_BONUS", GOM_BONUS)
+		.value("GOM_TECH", GOM_TECH)
 	;
 
 	python::enum_<BoolExprTypes>("BoolExprTypes")
 		.value("NO_BOOLEXPR", NO_BOOLEXPR)
 		.value("BOOLEXPR_HAS", BOOLEXPR_HAS)
 		.value("BOOLEXPR_AND", BOOLEXPR_AND)
+		.value("BOOLEXPR_OR", BOOLEXPR_OR)
 	;
 
 	python::class_<BoolExpr>("CyBoolExpr")
@@ -28,5 +31,11 @@ void CyBoolExprPythonInterface()
 		.def("getType", &BoolExprAnd::getType)
 		.def("getFirstExpr", &BoolExprAnd::getFirstExpr, python::return_value_policy<python::reference_existing_object>())
 		.def("getSecondExpr", &BoolExprAnd::getSecondExpr, python::return_value_policy<python::reference_existing_object>())
+	;
+	
+	python::class_<BoolExprOr, python::bases<BoolExpr> >("CyBoolExprOr")
+		.def("getType", &BoolExprOr::getType)
+		.def("getFirstExpr", &BoolExprOr::getFirstExpr, python::return_value_policy<python::reference_existing_object>())
+		.def("getSecondExpr", &BoolExprOr::getSecondExpr, python::return_value_policy<python::reference_existing_object>())
 	;
 }
