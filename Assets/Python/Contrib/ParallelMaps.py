745225efd8f0c6f20597b4b7c1a8011586bf0f70
raxo2222 <raxo8888@gmail.com>
2021-07-24 20:09:30 +0200

    This should be inside I think

--- Assets/Python/Contrib/ParallelMaps.py
+++ Assets/Python/Contrib/ParallelMaps.py
@@ -14,9 +14,10 @@ class ParallelMaps:
 		if DebugUtils.bDebugMode and BugEventManager.g_eventManager.bAlt:
 			i = argsList[1] -2
 			#if i in range(MapTypes.NUM_MAPS) \
-			if i > -1 and i < 10 and i != GC.getGame().getCurrentMap():
-				GC.switchMap(i)
+			if i > -1 and i < 10 and i != GC.getGame().getCurrentMap():				
 				if not GC.getMapByIndex(i).plotsInitialized():
+					GC.switchMap(i)
 					CvUtil.sendImmediateMessage("Initialized Map %d: %s" %(i, GC.getMapInfo(i).getDescription()))
 				else:
+					GC.switchMap(i)
 					CvUtil.sendImmediateMessage("Map %d: %s" %(i, GC.getMapInfo(i).getDescription()))
