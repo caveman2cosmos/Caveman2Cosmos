917c98ab6b3c10eea93c089814d042bf2f8f3db2
raxo2222 <raxo8888@gmail.com>
2021-07-26 22:15:19 +0200

    Added bCtrl to be able to switch to rest of maps

--- Assets/Python/Contrib/ParallelMaps.py
+++ Assets/Python/Contrib/ParallelMaps.py
@@ -1,4 +1,4 @@
-from CvPythonExtensions import CyGlobalContext #, MapTypes
+from CvPythonExtensions import CyGlobalContext, MapTypes
 import BugEventManager
 import CvUtil
 import DebugUtils
@@ -19,5 +19,15 @@ class ParallelMaps:
 				if not GC.getMapByIndex(i).plotsInitialized():
 					CvUtil.sendImmediateMessage("Initialized Map %d: %s" %(i, GC.getMapInfo(i).getDescription()))
 				else:
-					CvUtil.sendImmediateMessage("Map %d: %s" %(i, GC.getMapInfo(i).getDescription())) 
+					CvUtil.sendImmediateMessage("Map %d: %s" %(i, GC.getMapInfo(i).getDescription()))
+				GC.switchMap(i)
+
+		if BugEventManager.g_eventManager.bCtrl:
+			i = argsList[1] +7 #Maps 10 - 16 (ctrl+1 -> ctrl+6)
+			if i > -1 and i < MapTypes.NUM_MAPS and i != GC.getGame().getCurrentMap() \
+			and (DebugUtils.bDebugMode or GC.getMapByIndex(i).plotsInitialized()):
+				if not GC.getMapByIndex(i).plotsInitialized():
+					CvUtil.sendImmediateMessage("Initialized Map %d: %s" %(i, GC.getMapInfo(i).getDescription()))
+				else:
+					CvUtil.sendImmediateMessage("Map %d: %s" %(i, GC.getMapInfo(i).getDescription()))
 				GC.switchMap(i)
