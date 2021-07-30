28d22c6b7fdaf836ab8ea0d6ba969b98d0515524
raxo2222 <raxo8888@gmail.com>
2021-07-30 10:12:55 +0200

    Prevent switching when entering debug mode

--- Assets/Python/Contrib/ParallelMaps.py
+++ Assets/Python/Contrib/ParallelMaps.py
@@ -24,8 +24,8 @@ class ParallelMaps:
 
 		eKey = argsList[1]
 
-		if BugEventManager.g_eventManager.bAlt:
+		if BugEventManager.g_eventManager.bAlt and not BugEventManager.g_eventManager.bCtrl:
 			return _tryMapSwitch(eKey -2) # enum of key -2 gives the correct value for the number keys (0-9). I know enums should not be used this way.
 
-		elif BugEventManager.g_eventManager.bCtrl:
+		elif BugEventManager.g_eventManager.bCtrl and not BugEventManager.g_eventManager.bAlt:
 			return _tryMapSwitch(eKey +7) # Maps 10 - 16 (ctrl+1 -> ctrl+6)
