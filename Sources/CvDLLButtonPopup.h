8b9bc0deabc09a5a75ccfdda8554682030ca3e63
raxo2222 <raxo8888@gmail.com>
2021-08-21 14:01:04 +0200

    Removed notifivation about removed assets

--- Sources/CvDLLButtonPopup.h
+++ Sources/CvDLLButtonPopup.h
@@ -64,20 +64,11 @@ private:
 	bool launchFreeColonyPopup(CvPopup* pPopup, CvPopupInfo &info);
 	bool launchLaunchPopup(CvPopup* pPopup, CvPopupInfo &info);
 	bool launchFoundReligionPopup(CvPopup* pPopup, CvPopupInfo &info);
-	//	Koshling - added to allow user to choose game save format
 	bool launchGetSaveFormatPopup(CvPopup* pPopup, CvPopupInfo &info);
-	bool launchGetSaveInfoLostPopup(CvPopup* pPopup, CvPopupInfo &info);
-	// AIAndy - added to recommend modifier recalculation
 	bool launchModifierRecalculationPopup(CvPopup* pPopup, CvPopupInfo &info);
 	bool launchNameListPopup(CvPopup* pPopup, CvPopupInfo &info);
-	// ls612: Added City Go-To popup.
 	bool launchGoToCityPopup(CvPopup* pPopup, CvPopupInfo &info);
 
-/************************************************************************************************/
-/* Afforess	                  Start		 09/19/10                                               */
-/*                                                                                              */
-/*                                                                                              */
-/************************************************************************************************/
 	bool invasionPopup(CvPopup* pPopup, CvPopupInfo &info);
 	bool launchSelectShadowUnitPopup(CvPopup* pPopup, CvPopupInfo &info);
 	bool launchSelectDiscoveryTechPopup(CvPopup* pPopup, CvPopupInfo &info);
