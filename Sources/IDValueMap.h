b006e9d13511056c75306a1abdfe30f2bdfd1de0
MattOttawa <qqwweeaassddzzzxxxccc111@gmail.com>
2021-09-24 08:46:33 -0400

    switched array to IDValueMap

--- Sources/IDValueMap.h
+++ Sources/IDValueMap.h
@@ -232,6 +232,7 @@ typedef std::pair<BonusTypes, int> BonusModifier2;
 typedef std::pair<BuildingTypes, int> BuildingModifier2;
 typedef std::pair<TechTypes, int> TechModifier;
 typedef std::pair<UnitTypes, int> UnitModifier2;
+typedef std::pair<ReligionTypes, int> ReligionModifier;
 typedef std::pair<UnitCombatTypes, int> UnitCombatModifier2;
 
 typedef std::pair<TechTypes, int*> TechArray;
