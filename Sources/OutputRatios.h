070a71994267f29263431dadf01ce812f8fd2a1a
MattOttawa <qqwweeaassddzzzxxxccc111@gmail.com>
2021-12-23 22:14:02 -0500

    added default AI yield weight values to c2c ini file

--- Sources/OutputRatios.h
+++ Sources/OutputRatios.h
@@ -4,15 +4,19 @@
 #define CIV_OUTPUTRATIOS_H
 struct OutputRatios
 {
+	OutputRatios();
 	OutputRatios(int food, int production, int commerce);
 	void WeightOutputs(int foodWeight, int productionWeight, int commerceWeight);
 	void WeightFood(int foodWeight);
 	void WeightProduction(int productionWeight);
 	void WeightCommerce(int commerceWeight);
 	int CalculateOutputValue(int food, int production, int commerce) const;
+	static void readDefaultWeightsFromIniFile();
 	int food_ratio;
 	int production_ratio;
 	int commerce_ratio;
+private:
+	static YieldArray default_weight;
 };
 
 #endif
