41a821646bc0e9807d99141b3885c8317a0b8ef8
Toffer <toffer_krix-offer@hotmail.com>
2022-01-23 18:08:23 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/caveman2cosmos/caveman2cosmos""
    This reverts commit 385a520d02428f8e8a82b9e21db0a6f8d62716e3.

--- Sources/CvWorkerService.cpp
+++ Sources/CvWorkerService.cpp
@@ -38,7 +38,7 @@ bool CvWorkerService::ImproveBonus(CvUnitAI* unit, CvPlot* plot, int allowedMove
 	const int maxDistanceFromBorder = unit->getGroup()->getNumUnits() > 1 && unit->getGroup()->canDefend() ? GC.getAI_WORKER_MAX_DISTANCE_FROM_CITY_OUT_BORDERS() / 2 + 1 : -1;
 	BuildTypes overallBestBuild = NO_BUILD;
 	int bestBonusValue = 0;
-	int numberOfMoveTurns = 0;
+	int finalNumberOfMoveTurns = 0;
 	CvPlot* bestPlot = NULL;
 
 	CvReachablePlotSet plotSet(unit->getGroup(), iBasePathFlags, MAX_INT, true, maxDistanceFromBorder);
@@ -71,7 +71,7 @@ bool CvWorkerService::ImproveBonus(CvUnitAI* unit, CvPlot* plot, int allowedMove
 			BuildTypes tempPlotBuild = GetFastestBuildForImprovementType(ownerReference, potentialImprovementType, plot, false);
 
 			const int tempDefenseValue = (
-				1
+				1 // minimum 1 value for providing the bonus.
 				+ potentialImprovement->getAirBombDefense() / 10
 				+ potentialImprovement->getDefenseModifier() / 10
 				+ (gameOptionZoneOfControl && potentialImprovement->isZOCSource() ? 3 : 0)
@@ -88,22 +88,26 @@ bool CvWorkerService::ImproveBonus(CvUnitAI* unit, CvPlot* plot, int allowedMove
 		//const bool haveBonus = ownerReference.hasBonus(nonObsoleteBonusType);
 		//const bool plotIsConnected = loopedPlot->isConnectedToCapital(unitOwner);
 
+		const bool plotIsConnected = loopedPlot->isConnectedToCapital(unitOwner);
+
+		int numberOfMoveTurns = 0;
 		if (!unit->generatePath(loopedPlot, iBasePathFlags, false, &numberOfMoveTurns)) continue;
 
-		const int tempBonusValue = std::max(1, (ownerReference.AI_bonusVal(nonObsoleteBonusType) + bestDefenseValue) / std::max(1, numberOfMoveTurns));
+		const int tempBonusValue = std::max(1, ownerReference.AI_bonusVal(nonObsoleteBonusType) / std::max(1, numberOfMoveTurns));
 
 		/*if (numberOfMoveTurns <= allowedMovementTurns)*/ {
 			if (bestBonusValue < tempBonusValue) {
 				bestBonusValue = tempBonusValue;
 				overallBestBuild = bestBuildForPlot;
 				bestPlot = loopedPlot;
+				finalNumberOfMoveTurns = numberOfMoveTurns;
 			}
 		}
 	}
 	if (overallBestBuild == NO_BUILD) return false;
 	MissionTypes eBestMission = MISSION_MOVE_TO;
 
-	if (numberOfMoveTurns >= stepDistance(unit->getX(), unit->getY(), bestPlot->getX(), bestPlot->getY())) {
+	if (finalNumberOfMoveTurns >= stepDistance(unit->getX(), unit->getY(), bestPlot->getX(), bestPlot->getY())) {
 		eBestMission = MISSION_ROUTE_TO;
 	}
 
