950eea893ea88bb10c61ae5b5ac3103b5f862f1d
raxo2222 <raxo8888@gmail.com>
2022-01-24 20:42:06 +0100

    Deleted buggy debug code

--- Sources/CvGlobals.cpp
+++ Sources/CvGlobals.cpp
@@ -2919,10 +2919,6 @@ void cvInternalGlobals::setIsBug()
 
 	::setIsBug();
 
-#ifdef _DEBUG // Matt: temporary
-	Cy::call("CvInfoUtilInterface", "init");
-#endif
-
 	//	If viewports are truned on in BUG the settinsg there override those in the global defines
 	if (getBugOptionBOOL("MainInterface__EnableViewports", false))
 	{
