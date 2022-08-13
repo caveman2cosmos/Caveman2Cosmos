96398ba2fdb14151a7d5012408f5f51a2ce56803
Marco Bellmann <dsma_bell@web.de>
2022-08-13 10:26:14 +0200

    feat(Language): German translations
    * Deception and related buildings and units
    * more animals

--- Assets/Python/Screens/Pedia/PediaBuilding.py
+++ Assets/Python/Screens/Pedia/PediaBuilding.py
@@ -443,7 +443,7 @@ class PediaBuilding:
 			else:
 				bPlus = True
 			if aList3:
-				screen.attachLabel(panelName, "", szBracketL + szfont2b + "Own")
+				screen.attachLabel(panelName, "", szBracketL + szfont2b + TRNSLTR.getText("TXT_KEY_PEDIA_OWN", ()))
 				for i in range(len(aList3)):
 					iType, iAmount = aList3[i]
 					screen.attachLabel(panelName, "", szfont4b + " " + str(iAmount))
