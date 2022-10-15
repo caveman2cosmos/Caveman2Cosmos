dc12a4eea68e1400864d08af62c326190366f004
Toffer <toffer_krix-offer@hotmail.com>
2022-10-15 21:45:47 +0200

    Fixed bug in planet generator mapscript introduced when storm feature was removed.

--- PrivateMaps/C2C_Planet_Generator_0_68.py
+++ PrivateMaps/C2C_Planet_Generator_0_68.py
@@ -2741,12 +2741,6 @@ def addFeatures():
 			randomTerrain[7 * (climateHumidity / 2 + 0.75)] = terrainPermafrost
 
 		for x in range(iW):
-# Rise of Mankind 2.82 start
-			if ( y >= 5 and y <= (iH - 5)):
-				plot = map.plot(x,y)
-				if plot != 0 and plot.isWater() == True and getRand(dice) < stormChance:
-					plot.setFeatureType(featStorm,0)
-# Rise of Mankind 2.82 end
 
 			plot = map.sPlot(x,y)
 			if plot.isWater():
