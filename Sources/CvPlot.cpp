79daa9a4e6fa85e011f0fb76f8b7b0f637df4d5d
Toffer <toffer_krix-offer@hotmail.com>
2022-12-03 19:00:48 +0100

    Fixed bug with culture, didn't round up minimum culture output with high enough accuracy.

--- Sources/CvPlot.cpp
+++ Sources/CvPlot.cpp
@@ -10310,8 +10310,8 @@ int CvPlot::minimumNonDecayCulture()
 	return (
 		// Toffer - Round up (multiply by 100 add 99 and then divide all by 100) to get the effective min value.
 		(
-			99 +
-			(1 + GC.getTILE_CULTURE_DECAY_CONSTANT()) * 100000
+			999 +
+			(1 + GC.getTILE_CULTURE_DECAY_CONSTANT()) * 1000000
 			/
 			(
 				1000 - (
@@ -10321,7 +10321,7 @@ int CvPlot::minimumNonDecayCulture()
 				)
 			)
 		)
-		/ 100
+		/ 1000
 	);
 }
 
