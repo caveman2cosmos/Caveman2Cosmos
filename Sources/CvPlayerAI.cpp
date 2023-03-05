c30990784f406ebee3f92f5c6281ea5fe4dd47d0
Toffer <toffer_krix-offer@hotmail.com>
2023-03-05 03:16:33 +0100

    Insignificant changes to code comments.

--- Sources/CvPlayerAI.cpp
+++ Sources/CvPlayerAI.cpp
@@ -20419,10 +20419,10 @@ void CvPlayerAI::read(FDataStreamBase* pStream)
 
 	WRAPPER_READ_OBJECT_START(wrapper);
 
-	CvPlayer::read(pStream);	// read base class data first
+	CvPlayer::read(pStream); // read base class data first
 
 	uint uiFlag = 0;
-	WRAPPER_READ(wrapper, "CvPlayerAI", &uiFlag);	// flags for expansion
+	WRAPPER_READ(wrapper, "CvPlayerAI", &uiFlag); // flags for expansion
 
 	if ((uiFlag & PLAYERAI_UI_FLAG_OMITTED) == 0)
 	{
@@ -20589,28 +20589,15 @@ void CvPlayerAI::write(FDataStreamBase* pStream)
 
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_iStrategyHash);
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_iStrategyHashCacheTurn);
-		/************************************************************************************************/
-		/* BETTER_BTS_AI_MOD					  03/18/10								jdog5000	  */
-		/*																							  */
-		/* Victory Strategy AI																		  */
-		/************************************************************************************************/
+
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_iStrategyRand);
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_iVictoryStrategyHash);
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_iVictoryStrategyHashCacheTurn);
-		/************************************************************************************************/
-		/* BETTER_BTS_AI_MOD					   END												  */
-		/************************************************************************************************/
-		/************************************************************************************************/
-		/* RevDCM					  Start		 12/9/09												*/
-		/*																							  */
-		/* Inquisitions																				 */
-		/************************************************************************************************/
+
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_bPushReligiousVictory);
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_bConsiderReligiousVictory);
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_bHasInquisitionTarget);
-		/************************************************************************************************/
-		/* RevDCM						 END															*/
-		/************************************************************************************************/
+
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_iAveragesCacheTurn);
 		WRAPPER_WRITE(wrapper, "CvPlayerAI", m_iAverageGreatPeopleMultiplier);
 
