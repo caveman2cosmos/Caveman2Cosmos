6ec9b8170f8eb6f3782d23e84acc60c3094486fb
Toffer <toffer_krix-offer@hotmail.com>
2023-05-20 20:55:58 +0200

    Fixed a python error in barb civ code.

--- Assets/Python/Revolution/Gameready/BarbarianCiv.py
+++ Assets/Python/Revolution/Gameready/BarbarianCiv.py
@@ -247,7 +247,7 @@ class BarbarianCiv:
 			iMinEra = iEra - self.RevOpt.getNewWorldErasBehind()
 			if iMinEra > -1:
 				for iTech in xrange(iNumTechs):
-					if CyPlayer.canResearch(iTech, False) and GC.getTechInfo(iTech).getEra() <= iMinEra:
+					if CyPlayer.canResearch(iTech, False, True) and GC.getTechInfo(iTech).getEra() <= iMinEra:
 						CyTeam.setHasTech(iTech, True, iPlayer, False, False)
 		else:
 			iNumTeams = GAME.countCivTeamsAlive()
@@ -257,7 +257,7 @@ class BarbarianCiv:
 				if iTech in techsOwned:
 					CyTeam.setHasTech(iTech, True, iPlayer, False, False)
 					continue
-				if not CyPlayer.canResearch(iTech, False):
+				if not CyPlayer.canResearch(iTech, False, True):
 					continue
 				iKnownRatio = 100 * GAME.countKnownTechNumTeams(iTech) / iNumTeams
 				if iKnownRatio < 100 and closeTeams:
