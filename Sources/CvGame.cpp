0cad78855df2293879d11b1e820e2ac3cc88d0f6
Toffer <toffer_krix-offer@hotmail.com>
2024-03-21 00:09:29 +0100

    Fixed a big bug with animal spawn of different group sizes on size matters, now animals can spawn with merged up/down promotions applied. Also optimized spawn code quite a bit by not having it loop through all unitCombat types in the game when doing this merge/split promo process, now it just loops through the subcombat types of the animal in question until it finds the group size unitCombat defined for it which it then goes to replace.

--- Sources/CvGame.cpp
+++ Sources/CvGame.cpp
@@ -6036,11 +6036,11 @@ void enumSpawnPlots(const CvSpawnInfo& spawnInfo, std::vector<CvPlot*>* plots)
 namespace Game {
 	bool isGroupUpgradePromotion(const CvUnit* unit, PromotionTypes promotion)
 	{
-		return GC.getPromotionInfo(promotion).getGroupChange() > 0 && unit->canAcquirePromotion(promotion, PromotionRequirements::ForStatus);
+		return GC.getPromotionInfo(promotion).getGroupChange() > 0 && unit->canAcquirePromotion(promotion, PromotionRequirements::ForFree | PromotionRequirements::ForOffset);
 	}
 	bool isGroupDowngradePromotion(const CvUnit* unit, PromotionTypes promotion)
 	{
-		return GC.getPromotionInfo(promotion).getGroupChange() < 0 && unit->canAcquirePromotion(promotion, PromotionRequirements::ForStatus);
+		return GC.getPromotionInfo(promotion).getGroupChange() < 0 && unit->canAcquirePromotion(promotion, PromotionRequirements::ForFree | PromotionRequirements::ForOffset);
 	}
 }
 
@@ -6258,18 +6258,17 @@ void CvGame::doSpawns(PlayerTypes ePlayer)
 							//remove old group volume unitcombat
 							if (eGroupVolume != NO_UNITCOMBAT)
 							{
-								for (int iI = 0; iI < GC.getNumUnitCombatInfos(); iI++)
+								foreach_(const UnitCombatTypes eSubCombat, kUnit.getSubCombatTypes())
 								{
-									const UnitCombatTypes eUnitCombat = static_cast<UnitCombatTypes>(iI);
-
-									if (GC.getUnitCombatInfo(eUnitCombat).getGroupBase() != -10 && pUnit->isHasUnitCombat(eUnitCombat) && eUnitCombat != eGroupVolume)
+									if (GC.getUnitCombatInfo(eSubCombat).getGroupBase() != -10 && eSubCombat != eGroupVolume)
 									{
 										CvUnit::normalizeUnitPromotions(
 											pUnit,
-											GC.getUnitCombatInfo(eGroupVolume).getGroupBase() - GC.getUnitCombatInfo(eUnitCombat).getGroupBase(),
+											GC.getUnitCombatInfo(eGroupVolume).getGroupBase() - GC.getUnitCombatInfo(eSubCombat).getGroupBase(),
 											bind(Game::isGroupUpgradePromotion, pUnit, _2),
 											bind(Game::isGroupDowngradePromotion, pUnit, _2)
 										);
+										break;
 									}
 								}
 							}
