69823a0e93d90e096d2f7041b0eb4f3c8f2ae6da
Calvitix <calvitix@gmail.com>
2025-07-20 19:12:42 +0200

    -Removed some Maps +Add remove dictritics on savegame name (autosav) *Some fixes in French Textes *Tweak Cut Losers (15 civ, and eveyr 100 turns)

--- Assets/Python/Contrib/AutoSave.py
+++ Assets/Python/Contrib/AutoSave.py
@@ -4,6 +4,39 @@ GC = CyGlobalContext()
 GAME = GC.getGame()
 MAP = GC.getMap()
 
+
+def remove_diacritics(in_text):
+    # Table des remplacements (caractères + codes explicites)
+    print "Start remove_diacritics for %s" % in_text
+    print "%s" % str(type(in_text))
+    text = u''
+    try :
+		text = in_text.encode("utf-8")
+    except :
+		text = in_text.encode("ascii", "ignore")
+    print "%s" % text
+    text = text.replace('à', 'a').replace('â', 'a').replace('ä', 'a').replace('á', 'a').replace('ã', 'a').replace('å', 'a')
+    print "%s" % text
+    text = text.replace('ç', 'c')
+    text = text.replace('é', 'e').replace('è', 'e').replace('ê', 'e').replace('ë', 'e')
+    text = text.replace('î', 'i').replace('ï', 'i').replace('í', 'i')
+    text = text.replace('ô', 'o').replace('ö', 'o').replace('ò', 'o').replace('ó', 'o').replace('õ', 'o')
+    text = text.replace('ù', 'u').replace('û', 'u').replace('ü', 'u').replace('ú', 'u')
+    text = text.replace('ÿ', 'y')
+    text = text.replace('ñ', 'n')
+
+    text = text.replace('À', 'A').replace('Â', 'A').replace('Ä', 'A').replace('Á', 'A').replace('Ã', 'A').replace('Å', 'A')
+    text = text.replace('Ç', 'C')
+    text = text.replace('É', 'E').replace('È', 'E').replace('Ê', 'E').replace('Ë', 'E')
+    text = text.replace('Î', 'I').replace('Ï', 'I').replace('Í', 'I')
+    text = text.replace('Ô', 'O').replace('Ö', 'O').replace('Ò', 'O').replace('Ó', 'O').replace('Õ', 'O')
+    text = text.replace('Ù', 'U').replace('Û', 'U').replace('Ü', 'U').replace('Ú', 'U')
+    text = text.replace('Ÿ', 'Y')
+    text = text.replace('Ñ', 'N')
+
+    print "%s" % text
+    return text
+        
 def init():
 	import SystemPaths as SP
 	global _saveDir, options
@@ -50,6 +83,11 @@ def save(type, prefix, iTurn):
 	dir += '-' + GC.getHandicapInfo(CyPlayer.getHandicapType()).getText()[:5]
 	dir += ".CivBeyondSwordSave"
 
+	print "before AutoSave.savename : %s" % dir
+
+	dir = remove_diacritics(dir)
+
+	print "after AutoSave.savename : %s" % dir
 	GAME.saveGame(str(dir))
 	print "AutoSave.save\n\t%s" % dir
 
