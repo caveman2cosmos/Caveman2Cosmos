ba4108a32742e349df31b002a655b4530db1dff7
Calvitix <calvitix@gmail.com>
2025-07-25 22:43:39 +0200

    French Translation from 25.07.2025
    * Change the game options with prefix
    * Add some missing pedias
    *

--- Assets/Python/Contrib/AutoSave.py
+++ Assets/Python/Contrib/AutoSave.py
@@ -7,16 +7,16 @@ MAP = GC.getMap()
 
 def remove_diacritics(in_text):
     # Table des remplacements (caractères + codes explicites)
-    print "Start remove_diacritics for %s" % in_text
-    print "%s" % str(type(in_text))
+    #print "Start remove_diacritics for %s" % in_text
+    #print "%s" % str(type(in_text))
     text = u''
     try :
 		text = in_text.encode("utf-8")
     except :
 		text = in_text.encode("ascii", "ignore")
-    print "%s" % text
+    #print "%s" % text
     text = text.replace('à', 'a').replace('â', 'a').replace('ä', 'a').replace('á', 'a').replace('ã', 'a').replace('å', 'a')
-    print "%s" % text
+    #print "%s" % text
     text = text.replace('ç', 'c')
     text = text.replace('é', 'e').replace('è', 'e').replace('ê', 'e').replace('ë', 'e')
     text = text.replace('î', 'i').replace('ï', 'i').replace('í', 'i')
@@ -34,7 +34,7 @@ def remove_diacritics(in_text):
     text = text.replace('Ÿ', 'Y')
     text = text.replace('Ñ', 'N')
 
-    print "%s" % text
+    #print "%s" % text
     return text
         
 def init():
@@ -83,11 +83,11 @@ def save(type, prefix, iTurn):
 	dir += '-' + GC.getHandicapInfo(CyPlayer.getHandicapType()).getText()[:5]
 	dir += ".CivBeyondSwordSave"
 
-	print "before AutoSave.savename : %s" % dir
+	#print "before AutoSave.savename : %s" % dir
 
 	dir = remove_diacritics(dir)
 
-	print "after AutoSave.savename : %s" % dir
+	#print "after AutoSave.savename : %s" % dir
 	GAME.saveGame(str(dir))
 	print "AutoSave.save\n\t%s" % dir
 
